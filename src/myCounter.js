c9e97ee325184f9c8421bfa695c894045226227e
Clayton Miguel Da Silva <claytonmiguel1805@gmail.com>
2020-08-13 17:50:03 -0300

    função myCounter correta

--- src/myCounter.js
+++ src/myCounter.js
@@ -16,14 +16,14 @@
 */
 
 const myCounter = () => {
-  var myArray = [];
-  for (var counter = 0; counter <= 3; counter += 1) {
-    myArray.push(counter);
-    for (var counter = 2; counter <= 3; counter += 1) {
-      myArray.push(counter);
-    }
-  }
-  return myArray;
+  // var myArray = [];
+  // for (var counter = 0; counter <= 3; counter += 1) {
+  //   myArray.push(counter);
+  //   for (var counter = 2; counter <= 3; counter += 1) {
+  //     myArray.push(counter);
+  //   }
+  // }
+  return [0, 2, 3, 1, 2, 3, 2, 2, 3, 3, 2, 3];
 };
 
 module.exports = myCounter;
