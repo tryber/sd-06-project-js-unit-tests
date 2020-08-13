59af90422f354d0e9b5086403611a21dbef349cb
Pedro Risso <pmerchiorattor@gmail.com>
2020-08-13 20:16:21 -0300

    changed function to meet codeclimate requirements

--- src/average.js
+++ src/average.js
@@ -12,23 +12,18 @@
     - average([1, '2']) // Retorno: undefined;
 */
 
-
-const firstArray = [1, 2, 3, 4, 5];
-const secondArray = ['science', 'nature', 'css'];
-const number = 9;
-
 const average = (arr) => {
-  if (typeof(arr) !== 'object' || arr.length === 0) {
+  if (typeof (arr) !== 'object' || arr.length === 0) {
     return undefined;
   }
   sum = 0;
-  for (const item of arr) {
-    if (typeof(item) !== 'number') {
-      return undefined;    
-      }
-      sum += item;
+  for (let index = 0; index < arr.length; index += 1) {
+    if (typeof (arr[index]) !== 'number') {
+      return undefined;
     }
-  return(Math.round(sum/arr.length));
+    sum += arr[index];
+  }
+  return (Math.round(sum / arr.length));
 };
 
 module.exports = average;
