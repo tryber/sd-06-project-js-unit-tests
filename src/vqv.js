ccba56afd1711abaedd25514cce254dc1da44eb0
Macarena Rodriguez <macarenar697@gmail.com>
2020-08-13 16:56:00 -0300

    Arrumar codeclimate

--- src/vqv.js
+++ src/vqv.js
@@ -16,11 +16,12 @@
       #VQV!'
 */
 
-const vqv = (nome, idade) => { if (nome === undefined || idade === undefined) return undefined
-return `Oi, meu nome é ${nome}!
+const vqv = (nome, idade) =>
+{ if (nome === undefined || idade === undefined) return undefined;
+  return `Oi, meu nome é ${nome}!
 Tenho ${idade} anos,
 trabalho na Trybe e mando muito em programação!
-#VQV!`
+#VQV!`;
 };
 
 module.exports = vqv;
