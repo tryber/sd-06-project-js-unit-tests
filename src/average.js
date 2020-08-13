a2f6a8a7435be84c8a6689a74d756793d9312d92
Renan Cunha <renan2610@gmail.com>
2020-08-13 15:30:09 -0300

    iniciando o projeto Unit Tests :rocket: :corn:

--- src/average.js
+++ src/average.js
@@ -12,6 +12,17 @@
     - average([1, '2']) // Retorno: undefined;
 */
 
-const average = () => {};
+const average = (array) => {
+  if (array.length === 0) return undefined;
+
+  let sum = 0;
+  for (let index = 0; index < array.length; index += 1) {
+    if (typeof array[index] !== 'number') return undefined;
+    sum += array[index];
+  }
+  const media = sum / array.length;
+  return Math.round(media);
+};
+console.log(average([1, 2, 5]));
 
 module.exports = average;
