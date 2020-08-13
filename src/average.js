38b4db3d533e749d1905d0030e8653d9508df5ae
Maria Alice Guimaraes Carneiro <mguimaraes.carneiro@gmail.com>
2020-08-13 17:58:55 -0300

    fixing average function  codecliamte issues 02

--- src/average.js
+++ src/average.js
@@ -16,14 +16,13 @@ const average = (arr) => {
   let sum = 0;
   if (arr.length === 0) {
     return undefined;
-  } 
+  }
   for (let i = 0; i < arr.length; i += 1) {
     if (typeof arr[i] !== 'number') {
-    return undefined;
-    } 
-  sum += arr[i];
+      return undefined;
+    }
+    sum += arr[i];
   }
   return Math.round(sum / arr.length);
 };
-
 module.exports = average;
