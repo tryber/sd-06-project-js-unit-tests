30066680eaf12957b12a39902841906d50aad2b8
Maria Alice Guimaraes Carneiro <mguimaraes.carneiro@gmail.com>
2020-08-13 16:18:15 -0300

    1rst requirement

--- src/average.js
+++ src/average.js
@@ -12,6 +12,20 @@
     - average([1, '2']) // Retorno: undefined;
 */
 
-const average = () => {};
+const average = (arr) => {
+  let sum = 0;
+  let averageNumber = 0;
+  if (arr.length != 0){
+    for (let i = 0; i < arr.length; i += 1) {
+      if (typeof arr[i] !== 'number') {
+        return undefined;
+      } else {
+        sum += arr[i];
+      }
+    }
+    averageNumber = Math.round(sum/arr.length);
+    return averageNumber;
+  }
+};
 
 module.exports = average;
