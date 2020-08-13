2dba2e6ed73b6f60a2e098184c900fd8e2d7dcfd
Aloízio <aloiziobsc@gmail.com>
2020-08-13 19:48:15 -0300

    correção 5, requisito3

--- src/vqv.js
+++ src/vqv.js
@@ -20,6 +20,7 @@ const vqv = (nome, idade) =>
   `Oi, meu nome é ${nome}!
   Tenho ${idade} anos,
   trabalho na Trybe e mando muito em programação!
-  #VQV!`;
+  #VQV!
+  `;
 
 module.exports = vqv;
