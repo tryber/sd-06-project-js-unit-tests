62331f74ac7e2c867f91d35709d112386814a134
Clayton Miguel Da Silva <claytonmiguel1805@gmail.com>
2020-08-13 16:21:59 -0300

    função average funcional

--- src/average.js
+++ src/average.js
@@ -12,6 +12,19 @@
     - average([1, '2']) // Retorno: undefined;
 */
 
-const average = () => {};
+const average = (array) => {
+  if (array.length === 0) {
+    return 'undefined';
+  }
+  let somaArray = 0;
+  for (let index = 0; index < array.length; index += 1) {
+    if (typeof(array[index]) !== "number") {
+      return undefined;
+      break;
+    }
+    somaArray += array[index];
+  }
+  return Math.round(somaArray / array.length);
+};
 
 module.exports = average;
