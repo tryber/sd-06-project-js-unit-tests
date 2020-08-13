f3e5d4122088bf130c9761e679bea620665f232e
DehSartoreto <fixsartoreto@gmail.com>
2020-08-13 18:30:42 -0300

    obj calculator codeclimate

--- src/objCalculator.js
+++ src/objCalculator.js
@@ -20,10 +20,10 @@
 */
 
 const calculator = {
-  add: (num1, num2) => parseInt(num1 + num2),
-  mult: (num1, num2) => parseInt(num1 * num2),
-  div: (num1, num2) => parseInt(num1 / num2),
-  sub: (num1, num2) => parseInt(num1 - num2),
+  add: (num1, num2) => parseInt((num1 + num2), 10),
+  mult: (num1, num2) => parseInt((num1 * num2), 10),
+  div: (num1, num2) => parseInt((num1 / num2), 10),
+  sub: (num1, num2) => parseInt((num1 - num2), 10),
 };
 
 module.exports = calculator;
