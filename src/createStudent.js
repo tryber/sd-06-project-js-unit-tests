d660b765bf57f66948c801ad8ec8380f682d75c8
ricardorosa-dev <ricardorosa@gmail.com>
2020-08-13 19:16:30 -0300

    codeclimate

--- src/createStudent.js
+++ src/createStudent.js
@@ -16,10 +16,7 @@
 */
 
 const createStudent = (nome) => {
-  return {
-    name: nome,
-    feedback: () => 'Eita pessoa boa!',
-  };
+  return { name: nome, feedback: () => 'Eita pessoa boa!', };
 };
 
 module.exports = createStudent;
