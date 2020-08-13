59b90b275d0a583e2ecf55914eb3b0876d524669
Dilenio Enderle <dilenio@gmail.com>
2020-08-13 16:18:04 -0300

    1 - Implemente a função do arquivo src/average.js

--- src/average.js
+++ src/average.js
@@ -12,6 +12,25 @@
     - average([1, '2']) // Retorno: undefined;
 */
 
-const average = () => {};
+const average = (arrayNumbers) => {
+  let sum = 0;
+  let average = 0;
+  if (arrayNumbers.length === 0) {
+    return undefined;
+  }
+  for (let index = 0; index < arrayNumbers.length; index += 1) {
+    if (typeof arrayNumbers[index] === 'string') {
+      return undefined;
+    }
+    sum += arrayNumbers[index];
+  }
+  average = (sum / arrayNumbers.length);
+  if (average > 0 && average < 1) {
+    return 0;
+  } else if (average < 0) {
+    return Math.round(average);
+  }
+  return Math.trunc(average);
+};
 
 module.exports = average;
