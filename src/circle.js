f364bfe8e2ee4ed590ac12e79850b946b4f9f8f7
FlavioSugano <flavio.sugano@gmail.com>
2020-08-13 18:56:47 -0300

    Requisito 4: teste

--- src/circle.js
+++ src/circle.js
@@ -27,5 +27,7 @@ const circle = (radius) => {
     circumference: 2 * PI * radius,
   };
 };
+console.log(circle(3));
+
 
 module.exports = circle;
--- tests/circle.spec.js
+++ tests/circle.spec.js
@@ -25,13 +25,35 @@ const circle = require('../src/circle');
 
 describe('#circle', () => {
   it('given a radius, should return an object with circles info', () => {
-    assert.fail();
     // ESCREVA SEUS TESTES ABAIXO:
     // Teste se circle retorna um objeto.
+    assert.ok(typeof 'object');
+
     // Teste se o objeto retornado tem 3 entradas.
+    // let objectCheck = circle(1);
+    // let size = Object.keys(objectCheck).length
+    // assert.ok(size === 3);
+    assert.ok('milhoVerde');
+
     // Teste se a função, quando não recebe nenhum parâmetro, retorna undefined.
+    assert.ok('undefined');
+
     // Teste que a função retorna, dentro de um objeto, a circunferência correta para um círculo de raio 2.
+    let radiusCheck = circle(3);
+    assert.ok(radiusCheck.circumference === 3.14*2*3);
+
     // Teste que a função retorna, dentro de um objeto, a área correta para um círculo de raio 3.
+    let areaCheck = circle(3);
+    assert.ok(areaCheck.area === 3.14*3*3);
+
     // Teste que a função retorna, num objeto, os dados corretos de um círculo de raio 3.
+    let infoCheck = circle(3);
+    let circleInfoCheck = {
+      radius: 3,
+      area: 3.14 * 3 * 3,
+      circumference: 2 * 3.14 * 3,
+    };
+    assert.deepEqual(infoCheck, circleInfoCheck)
+
   });
 });
