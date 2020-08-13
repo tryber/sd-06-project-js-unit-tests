586a31ab49a3fc50406d78cd1fd7d1587fd3f2e0
CristianoNunes <cristianonunes99@gmail.com>
2020-08-13 18:24:18 -0300

    Requisito 1 Correcao8 CodeClimate

--- src/average.js
+++ src/average.js
@@ -15,7 +15,7 @@
 const average = (arr) => {
   let sum = 0;
   if (arr.length === 0) {
-    return undefined
+    return undefined;
   }
   for (let i = 0; i < arr.length; i += 1) {
     if (typeof arr[i] === 'string') {
