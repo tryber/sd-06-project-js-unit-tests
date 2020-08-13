5824adbe1521f6c8bd375853e069abd0189dc601
Elcio Rodrigo <e.rhodrigo.a@gmail.com>
2020-08-13 16:14:57 -0300

    vqv

--- src/vqv.js
+++ src/vqv.js
@@ -16,6 +16,16 @@
       #VQV!'
 */
 
-const vqv = (nome, idade) => {};
+const vqv = (nome, idade) => {
+  let phrase = '';
+  phrase = `Oi, meu nome é ${nome}!
+  Tenho ${idade} anos,
+  trabalho na Trybe e mando muito em programação!
+  #VQV!`
+  if (nome === '' && idade === '') {
+    return undefined;
+  }
+  return phrase;
+};
 
 module.exports = vqv;
