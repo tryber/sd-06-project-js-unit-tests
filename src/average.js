58de28664a99d4fe0823c174737088872d235ece
kissyla <kissylamottapereira@gmail.com>
2020-08-13 17:17:16 -0300

    primeiro commit VQV

--- src/average.js
+++ src/average.js
@@ -12,6 +12,21 @@
     - average([1, '2']) // Retorno: undefined;
 */
 
-const average = () => {};
-
+const average = (array) => {
+  let sum = 0;
+  if (array.length === 0) {
+    return undefined;
+  }
+    for (let index = 0; index < array.length; index += 1) {
+      if (typeof (array[index]) === "number") {
+        sum += array[index];
+      } else {
+        return undefined;
+      }
+    }
+  let media = sum / array.length;
+  let mediaRound = Math.round(media);
+  return mediaRound
+ };
+ 
 module.exports = average;
