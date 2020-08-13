ee7c254c81ddb652f95ef735619ebd8798d014c9
RenatoCostaDev <renatotrybe@gmail.com>
2020-08-13 18:06:12 -0300

    03- comitando o average.js passed

--- src/average.js
+++ src/average.js
@@ -1,5 +1,5 @@
 /*
-  A função average recebe um array (tamanho variável) e retorna a média dos valores recebidos.
+  A função average recebe um array (tamanho variável) numbers retorna a média dos valores recebidos.
   Caso a função receba algum valor não númerico ou um array vazio,
   o valor undefined deve ser retornado.
   Todos os resultados devem ser arredondados para valores inteiros. Ex: 4,6 vira 5; 1,3 vira 1.
@@ -12,20 +12,15 @@
     - average([1, '2']) // Retorno: undefined;
 */
 
-const average = (e) => {
+const average = (numbers) => {
   let sumArray = 0;
   let avgArray = 0;
-  // let numOfNotEqualZeroItens = 0;
-  for (let r = 0; r < e.length; r += 1) {
-    sumArray += e[r];
+  for (let r = 0; r < numbers.length; r += 1) {
+    sumArray += numbers[r];
   }
-  if (e.length === 0 || typeof (sumArray) === 'string') return undefined;
-  avgArray = Math.round(sumArray / e.length);
+  if (numbers.length === 0 || typeof (sumArray) === 'string') return undefined;
+  avgArray = Math.round(sumArray / numbers.length);
   return avgArray;
 };
-console.log(average([1, 2]));
-console.log(average([1, 2, 3, 4, 5]));
-console.log(average([1, 2, '3']));
-console.log(average([]));
-console.log(average([0, 0, 0, 0, 0, 0, 0]));
+
 module.exports = average;
