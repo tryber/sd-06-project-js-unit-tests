d33533bb37c2f0a08a8c6e669e8fd6bad0647646
MCoutinho <matheuscavalcante88@hotmail.com>
2020-08-13 18:21:59 -0300

    createStudent function done

--- src/createStudent.js
+++ src/createStudent.js
@@ -15,6 +15,14 @@
     estudante.feedback() // Retorna: 'Eita pessoa boa!'
 */
 
-const createStudent = () => {};
+const createStudent = (name) => {
+  const estudante = {
+    name,
+    feedback() {
+      return 'Eita pessoa boa!';
+    },
+  };
+  return estudante;
+};
 
 module.exports = createStudent;
