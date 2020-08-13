3ef24896eac12c2aab17975e24b632ac466750cc
Igor Leão <igorleao.eng@gmail.com>
2020-08-13 16:34:15 -0300

    code climate

--- src/average.js
+++ src/average.js
@@ -14,9 +14,8 @@
 
 const average = (array) => {
   let soma = 0;
-  let media = undefined;
-  
-  for(let i = 0; i < array.length ; i += 1) {
+  let media = 0;
+  for (let i = 0; i < array.length; i += 1) {
     if (typeof array[i] !== 'number') {
       return undefined;
     }
