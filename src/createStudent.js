1e2b523b9f301e1f5e390fa58019d1604a40ec81
Elcio Rodrigo <e.rhodrigo.a@gmail.com>
2020-08-13 18:15:40 -0300

    create student

--- src/createStudent.js
+++ src/createStudent.js
@@ -15,9 +15,9 @@
     estudante.feedback() // Retorna: 'Eita pessoa boa!'
 */
 
-const createStudent = (nome) => ({
-  name: nome,
-  feedback: () => 'Eita pessoa boa!',
-});
+const createStudent = (nome) => {
+  const person = { name: nome, feedback: () => 'Eita pessoa boa!' };
+  return person;
+};
 
 module.exports = createStudent;
