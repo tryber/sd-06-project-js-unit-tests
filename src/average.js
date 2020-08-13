856eecf5fbc9b9dfc3dbee5f5016c5dc48664bda
Marthus Sera <marthus2001@gmail.com>
2020-08-13 16:23:57 -0300

    Requisito 1

--- src/average.js
+++ src/average.js
@@ -13,6 +13,22 @@
     - average([1, '2']) // Retorno: undefined;
 */
 
-const average = () => {};
+const average = (arr) => {
+  let sum = 0;
+
+  if (arr.length === 0) {
+    return undefined;
+  }
+
+  for (let i = 0; i < arr.length; i++) {
+    if (typeof arr[i] === 'number') {
+      sum += arr[i];
+    } else {
+      return undefined;
+    }
+  }
+
+  return Math.round(sum / arr.length);
+};
 
 module.exports = average;
