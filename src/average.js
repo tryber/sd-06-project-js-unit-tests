5e9afb6a3caf6b7ad1895889d88cbbb10b9cb187
Pedro Duarte <ikbenpedro@gmail.com>
2020-08-13 19:12:46 -0300

    primeiro requisito code climate

--- src/average.js
+++ src/average.js
@@ -12,21 +12,21 @@
     - average([1, '2']) // Retorno: undefined;
 */
 
-const average = (array)=> {
+const average = (array) => {
   let sum = 0;
-  if (array.length===0) {
+  if (array.length === 0) {
     return undefined;
   }
-  for (let index=0;index<array.length;index+=1) {
-    if (typeof array[index]!=='number') {
+  for (let index = 0; index<array.length; index+=1) {
+    if (typeof array[index] !== 'number') {
       return undefined;
     }
     sum += array[index];
   }
-  return Math.round(sum/array.length);
+  return Math.round(sum / array.length);
 };
 
 
-// codigo baseado no link https://stackoverflow.com/questions/29544371/finding-the-average-of-an-array-using-js#:~:text=12%20Answers&text=You%20calculate%20an%20average%20by,var%20avg%20%3D%20total%20%2F%20grades. 
+/*codigo baseado no link https://stackoverflow.com/questions/29544371/finding-the-average-of-an-array-using-js#:~:text=12%20Answers&text=You%20calculate%20an%20average%20by,var%20avg%20%3D%20total%20%2F%20grades. */
 
 module.exports = average;
