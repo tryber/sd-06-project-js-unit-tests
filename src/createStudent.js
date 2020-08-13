9da739e33b6164999b93daaaed8658ed643875c5
Renan Cunha <renan2610@gmail.com>
2020-08-13 19:13:12 -0300

    corrigindo erro do codeclimate - requisito 5

--- src/createStudent.js
+++ src/createStudent.js
@@ -18,7 +18,7 @@
 const createStudent = (string) => {
   const student = {
     name: string,
-    feedback: ()  => 'Eita pessoa boa!',
+    feedback: () => 'Eita pessoa boa!',
   };
 
   return student;
