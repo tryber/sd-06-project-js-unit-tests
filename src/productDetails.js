3309495cb89dbc5b74730ec54bbb6173f7097e7e
Leandro Parisi <lepcbelisario@gmail.com>
2020-08-13 19:40:50 -0300

    requisito 6

--- src/productDetails.js
+++ src/productDetails.js
@@ -40,3 +40,12 @@ const productDetails = (firstProduct, secondProduct) => [
 ];
 
 module.exports = productDetails;
+
+// const assert = require ('assert');
+// assert.equal(typeof (productDetails('teste', 'teste')), 'object', 'Function result should be an Array');
+// assert.equal(productDetails('tes', 'tes').length, 2, 'array should have 2 items');
+// for (index = 0; index <= 1; index += 1) {
+//   assert.equal(typeof (productDetails('tes', 'tes')[index]), 'object', 'each of returned array indexes should be an object');
+// }
+// assert.notEqual(productDetails('tes', 'tes')[0], productDetails('tes', 'tes')[1], 'Objects should not be equal');
+// assert.match(productDetails('tes', 'tes')[0].details.productId, /^.*123$/, 'ProductID should end with 123');
--- tests/productDetails.spec.js
+++ tests/productDetails.spec.js
@@ -33,12 +33,19 @@ const productDetails = require('../src/productDetails');
 
 describe('#productDetails', () => {
   it('tests the function has the correct behaviour', () => {
-    assert.fail();
+    // assert.fail();
     // ESCREVA SEUS TESTES ABAIXO:
     // Teste que o retorno da função é um array.
+    assert.equal(typeof (productDetails('tes', 'tes')), 'object', 'Function result should be an Array');
     // Teste que o array retornado pela função contém dois itens dentro.
+    assert.equal(productDetails('tes', 'tes').length, 2, 'array should have 2 items');
     // Teste que os dois itens dentro do array retornado pela função são objetos.
+    for (index = 0; index <= 1; index += 1) {
+      assert.equal(typeof (productDetails('tes', 'tes')[index]), 'object', 'each of returned array indexes should be an object');
+    }
     // Teste que os dois objetos são diferentes entre si.
+    assert.notEqual(productDetails('tes', 'tes')[0], productDetails('tes', 'tes')[1], 'Objects should not be equal');
     // (Difícil) Teste que os dois productIds terminam com 123.
+    assert.match(productDetails('tes', 'tes')[0].details.productId, /^.*123$/, 'ProductID should end with 123');
   });
 });
