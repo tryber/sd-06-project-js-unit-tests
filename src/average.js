399c7b20cf4badb562d715f88e942473e6fc839f
Macarena Rodriguez <macarenar697@gmail.com>
2020-08-13 15:58:37 -0300

    Requisito um passed.... corroborar codeclimate

--- src/average.js
+++ src/average.js
@@ -12,8 +12,19 @@
     - average([1, '2']) // Retorno: undefined;
 */
 
-const average = () => {
-  
+const average = (arrayNumbers) => {
+  let summation = 0;
+  if (arrayNumbers.length === 0) {
+    return undefined;
+  }
+  for (let i = 0; i < arrayNumbers.length; i += 1) {
+    if (typeof arrayNumbers[i] !== 'number') {
+      return undefined;
+    }
+    summation += arrayNumbers[i];
+  }
+  let result = Math.round(summation / arrayNumbers.length);
+  return result;
 };
 
 module.exports = average;
