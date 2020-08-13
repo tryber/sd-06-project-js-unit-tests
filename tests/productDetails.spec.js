00278ade3c94d2d495f0c177cb925d318e6fd724
rafaelgelatti <rafael.gelatti@gmail.com>
2020-08-13 19:14:09 -0300

    trying nr6, very hard

--- tests/productDetails.spec.js
+++ tests/productDetails.spec.js
@@ -33,12 +33,25 @@ const productDetails = require('../src/productDetails');
 
 describe('#productDetails', () => {
   it('tests the function has the correct behaviour', () => {
-    assert.fail();
+    // assert.fail();
     // ESCREVA SEUS TESTES ABAIXO:
     // Teste que o retorno da função é um array.
+    assert.strictEqual(Array.isArray(productDetails('string', 'string')), true);
     // Teste que o array retornado pela função contém dois itens dentro.
+    assert.strictEqual(productDetails('string', 'string').length, 2);
     // Teste que os dois itens dentro do array retornado pela função são objetos.
+    assert.deepStrictEqual(typeof productDetails('string', 'string'), 'object');
     // Teste que os dois objetos são diferentes entre si.
+    assert.notStrictEqual(productDetails('strin1', 'string2')[0].details.productId, productDetails('strin1', 'string2')[1].details.productId);
     // (Difícil) Teste que os dois productIds terminam com 123.
+    // Eu tinha a ideia na cabeça de pegar os últimos 03 chars de uma string e comparar eles, mas não sabia como
+    // Aprendi aqui: https://www.techiedelight.com/get-last-n-characters-from-string-javascript/
+    // A lógica eu implementei sozinho a partir do 'substring'
+    const output = productDetails('string1', 'string2');
+    const obj1Id = output[0].details.productId;
+    const obj2Id = output[1].details.productId;
+    const obj1LastThree = obj1Id.substring(obj1Id.length - 3);
+    const obj2LastThree = obj2Id.substring(obj2Id.length - 3);
+    assert.strictEqual(obj1LastThree, obj2LastThree);
   });
 });
