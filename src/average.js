1c6556818a876f7954543cc929aa16dce67d5577
Vinicius Buzato <viniciusbuzato@gmail.com>
2020-08-13 18:34:45 -0300

    Step 1 - Done

--- src/average.js
+++ src/average.js
@@ -12,6 +12,19 @@
     - average([1, '2']) // Retorno: undefined;
 */
 
-const average = () => {};
+const average = (array) => {
+  //Math.round(array.reduce((a, b) => a + b) / array.length);
+  if (array.length === 0) return undefined;
+  let sum = 0;
+  for (let i = 0; i < array.length; i += 1) {
+    if (typeof array[i] !== 'number') {
+      return undefined;
+    }
+    sum += array[i];
+  }
+  return Math.round(sum / array.length);
+};
 
+
+//console.log(average([1, 2]));
 module.exports = average;
