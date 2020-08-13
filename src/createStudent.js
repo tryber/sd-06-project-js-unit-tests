932de2705b44b44b6c237460f16e03436f39198c
Elias Carvalho <eliascarvalho01@hotmail.com>
2020-08-13 18:18:52 -0300

    Fixing codeclimate

--- src/createStudent.js
+++ src/createStudent.js
@@ -15,10 +15,10 @@
     estudante.feedback() // Retorna: 'Eita pessoa boa!'
 */
 
-const createStudent = (name) => {
+const createStudent = name => {
   return {
     name,
-    feedback: () => { return 'Eita pessoa boa!'; },
+    feedback: _ => { return 'Eita pessoa boa!'; },
   };
 };
 
