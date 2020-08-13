f96f859e78e36bbabb6fb117abc6191e2668ae0b
rafaelgelatti <rafael.gelatti@gmail.com>
2020-08-13 19:50:34 -0300

    trying nr8

--- src/myCounter.js
+++ src/myCounter.js
@@ -17,10 +17,10 @@
 
 const myCounter = () => {
   var myArray = [];
-  for (var counter = 0; counter <= 3; counter += 1) {
-    myArray.push(counter);
-    for (var counter = 2; counter <= 3; counter += 1) {
-      myArray.push(counter);
+  for (let x = 0; x <= 3; x += 1) {
+    myArray.push(x);
+    for (let y = 2; y <= 3; y += 1) {
+      myArray.push(y);
     }
   }
   return myArray;
