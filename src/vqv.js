825a73879f35eb16291e7416e27e0254a6bb2566
DehSartoreto <fixsartoreto@gmail.com>
2020-08-13 16:42:56 -0300

    vqv function

--- src/vqv.js
+++ src/vqv.js
@@ -19,9 +19,9 @@
 const vqv = (nome, idade) => {
   if (nome !== undefined && idade !== undefined) {
     return `Oi, meu nome é ${nome}!\nTenho ${idade} anos,\ntrabalho na Trybe e mando muito em programação!\n#VQV!`;
-  } else {
-    return undefined;
   }
+  
+  return undefined;
 };
 
 module.exports = vqv;
