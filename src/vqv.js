314b9243e3ddbd1f29e63bb8ee0524ec95aed459
Daniel Menescal <danielmjales@bct.ect.ufrn.br>
2020-08-13 16:47:35 -0300

    add createStudent function

--- src/vqv.js
+++ src/vqv.js
@@ -23,7 +23,7 @@ const vqv = (nome, idade) => {
   message = `Oi, meu nome é ${nome}!
 Tenho ${idade} anos,
 trabalho na Trybe e mando muito em programação!
-#VQV!`
+#VQV!`;
   return message;
 };
 
