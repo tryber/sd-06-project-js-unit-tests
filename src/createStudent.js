2ce3c2df941aee61fe2d45ff6f755f798109bf33
Paulo Afonso Volpin <paulo.volpin@gmail.com>
2020-08-13 17:19:33 -0300

    Task 5: function constructed on file test/createStudent.js

--- src/createStudent.js
+++ src/createStudent.js
@@ -15,6 +15,6 @@
     estudante.feedback() // Retorna: 'Eita pessoa boa!'
 */
 
-const createStudent = () => {};
+const createStudent = (name) => ({name: name, feedback: () => 'Eita pessoa boa!'});
 
 module.exports = createStudent;
