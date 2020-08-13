a51a49cb9afdffe56130314ff880739db31e0cb7
Renan Cunha <renan2610@gmail.com>
2020-08-13 17:13:29 -0300

    adicionando requisito 3

--- src/average.js
+++ src/average.js
@@ -16,11 +16,13 @@ const average = (array) => {
   if (array.length === 0) return undefined;
 
   let sum = 0;
+
   for (let index = 0; index < array.length; index += 1) {
     if (typeof array[index] !== 'number') return undefined;
     sum += array[index];
   }
   const media = sum / array.length;
+
   return Math.round(media);
 };
 console.log(average([1, 2, 5]));
