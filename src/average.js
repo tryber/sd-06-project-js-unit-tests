2d407f2396ee0ae112e8cbe68a187fac9aaf9732
gussta <saideles.silveira@gmail.com>
2020-08-13 19:05:12 -0300

    average feito, talvez com complexidade

--- src/average.js
+++ src/average.js
@@ -12,6 +12,16 @@
     - average([1, '2']) // Retorno: undefined;
 */
 
-const average = () => { };
-//adicionando algo para abrir PR
+const average = (array) => {
+  let sum = 0;
+  if (array.length !== 0) {
+    for (let i = 0; i < array.length; i += 1) {
+      if (typeof array[i] === 'number') {
+        sum += array[i];
+      } else { return undefined; }
+    }
+  } else { return undefined; }
+  return Math.round(sum / array.length);  
+};
+
 module.exports = average;
