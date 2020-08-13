01b816c5236ce59f24e5e298eed4a65cf46caa13
Lais Gurgel <laisgurgelf@gmail.com>
2020-08-13 17:27:21 -0300

    Resoluçao numbers e circle

--- src/average.js
+++ src/average.js
@@ -4,7 +4,7 @@
   o valor undefined deve ser retornado.
   Todos os resultados devem ser arredondados para valores inteiros. Ex: 4,6 vira 5; 1,3 vira 1.
 
-  Parâmetros: 
+  Parâmetros:
     - Um array. Exemplos: [1, 2]; [1, 2, 3, 4, 5]; [1, 2, '3']; [];
   Comportamento:
     - average([2, 2]) // Retorno: 2;
@@ -12,6 +12,10 @@
     - average([1, '2']) // Retorno: undefined;
 */
 
-const average = () => {};
+const average = () => {
+
+};
+
+average([1, 2, 3]);
 
 module.exports = average;
