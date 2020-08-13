3e5a17f3ca69e2d6cbf98254c405e81f766f8c21
Clayton Miguel Da Silva <claytonmiguel1805@gmail.com>
2020-08-13 20:38:43 -0300

    code climat 7

--- src/createStudent.js
+++ src/createStudent.js
@@ -17,7 +17,9 @@
 
 const createStudent = (name) => {
   name,
-  feedback: () => {'Eita pessoa boa!'},
+  feedback: () => {
+    return 'Eita pessoa boa!';
+  },
 };
 // console.log(createStudent('clayton').feedback());
 
