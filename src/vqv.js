190f301b61fe17081ac8c308690ad0660ef3409c
Leandro Parisi <lepcbelisario@gmail.com>
2020-08-13 18:30:10 -0300

    Req 3

--- src/vqv.js
+++ src/vqv.js
@@ -16,6 +16,16 @@
       #VQV!'
 */
 
-const vqv = (nome, idade) => {};
+const vqv = (name, age) => {
+  if (name === undefined || age === undefined) {
+    return undefined;
+  }
+  let outputMessage = 
+  `Oi, meu nome é ${name}!
+Tenho ${age} anos,
+trabalho na Trybe e mando muito em programação!
+#VQV!`
+  return outputMessage;
+}
 
 module.exports = vqv;
