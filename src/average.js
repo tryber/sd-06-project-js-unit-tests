81475f565e12cb09752fec78f6a17df1e55852a9
Fábio Senra Corrêa <fabiosenracorrea@hotmail.com>
2020-08-13 16:52:36 -0300

    complexity 1 test

--- src/average.js
+++ src/average.js
@@ -16,21 +16,18 @@ const average = (arr) => {
   if (arr.includes(arr[0])) {
     let valid = true;
 
-    // copia o array para que não alteremos o de entrada (princípio de uma função)
     const sum = arr.slice(0).reduce((start, next, index, array) => {
-      if (typeof(next) !== "number") {
+      if (typeof next !== 'number') {
         valid = false;
-        array.splice(1); // 'quebra' o array que o reduce tá olhando, como se fosse um 'break' do loop.
       }
       return start + next;
-    }, 0)
+    }, 0);
 
     if (valid) {
       return Math.round(sum / arr.length);
     }
   }
+  return undefined;
 };
 
 module.exports = average;
-
-console.log(average([2,4, 's', 5]));
