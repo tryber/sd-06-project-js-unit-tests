56bf1eb243e67d74dbfbb28605707ea198fc2bc9
VinicinBraga <vinicius@bragamatta.com.br>
2020-08-13 17:22:56 -0300

    Solucionando novamente um  erro no codigo(CodeClimate)

--- src/average.js
+++ src/average.js
@@ -12,13 +12,12 @@
     - average([1, '2']) // Retorno: undefined;
 */
 
-const average = (array) => {
-  
+const average = (array) => { 
   let result = 0;
   if (array.length === 0) {
     return undefined;
   }
-  for (let i = 0; i < array.length; i++) {
+  for (let i = 0; i < array.length; i += 1) {
     if (typeof array[i] !== 'number') {
       return undefined;
     }
