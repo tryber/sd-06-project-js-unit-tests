48be60e51656da816b547633e1db28f28a9ae62a
Bruno Lemos <brunolemos.ssa@gmail.com>
2020-08-13 15:57:25 -0300

    Correção de sintaxe na função typeof

--- src/average.js
+++ src/average.js
@@ -21,7 +21,7 @@ const average = (array) => {
   }
   for (let i = 0; i < array.length; i += 1) {
     const arrayelement = array[i];
-    if (typeof (arrayelement) !== 'number') {
+    if (typeof arrayelement !== 'number') {
       return undefined;
     }
     sum += arrayelement;
