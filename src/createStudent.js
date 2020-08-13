048996514014a309955b0ba9e5b1edbf99309c1f
Elcio Rodrigo <e.rhodrigo.a@gmail.com>
2020-08-13 17:33:23 -0300

    create student

--- src/createStudent.js
+++ src/createStudent.js
@@ -18,7 +18,7 @@
 const createStudent = (nome) => {
   return {
     name: nome,
-    feedback: () => {return 'Eita pessoa boa!'}
+    feedback: () => {return ['Eita pessoa boa!']}
 };
 
 module.exports = createStudent;
