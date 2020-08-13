42b991c160aef4d69c8d0ac50d4d3bf40cfbfac7
AlexanderFeitosa <alexander.feitosa@gmail.com>
2020-08-13 19:37:27 -0300

    Project JS Unit Tests : Sixth Task

--- tests/productDetails.spec.js
+++ tests/productDetails.spec.js
@@ -33,12 +33,21 @@ const productDetails = require('../src/productDetails');
 
 describe('#productDetails', () => {
   it('tests the function has the correct behaviour', () => {
-    assert.fail();
+    //assert.fail();
     // ESCREVA SEUS TESTES ABAIXO:
     // Teste que o retorno da função é um array.
+    /*****************************************************************************************************
+     * Não me lembrava como verificar se um Objeto é um Array. Após "Googlada", obtive informações aqui :
+     * https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Reference/Global_Objects/Array/isArray
+     ****************************************************************************************************/
+    assert.equal(Array.isArray(productDetails('um', 'dois')), true);
     // Teste que o array retornado pela função contém dois itens dentro.
+    assert.equal(productDetails('um','dois').length, 2);
     // Teste que os dois itens dentro do array retornado pela função são objetos.
+    assert.equal(typeof(productDetails('um', 'dois')[0]) && typeof(productDetails('um', 'dois')[1]), 'object');
     // Teste que os dois objetos são diferentes entre si.
+    assert.notEqual(productDetails('um', 'dois')[0], productDetails('um', 'dois')[1]);
     // (Difícil) Teste que os dois productIds terminam com 123.
+    assert.notEqual(productDetails('um', 'dois')[0].details.productId.indexOf('123') && productDetails('um', 'dois')[1].details.productId.indexOf('123'), -1);
   });
 });
