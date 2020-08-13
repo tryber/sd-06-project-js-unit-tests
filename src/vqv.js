77b0ce18312fe17e7615ef62cb805705fab74605
Alessandra <itsalematos@hotmail.com>
2020-08-13 17:43:18 -0400

    Atualizando code climate

--- src/vqv.js
+++ src/vqv.js
@@ -15,14 +15,11 @@
       trabalho na Trybe e mando muito em programação!
       #VQV!'
 */
-
 const vqv = (nome, idade) => {
-  
-  if (nome === undefined || idade === undefined){
+  if (nome === undefined || idade === undefined) {
     return undefined;
   }
-
-    return `Oi, meu nome é ${nome}!
+  return `Oi, meu nome é ${nome}!
 Tenho ${idade} anos,
 trabalho na Trybe e mando muito em programação!
 #VQV!`;
