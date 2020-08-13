7079097521a4ad333f41cc9523fde04268ebcd2b
wfelipefarina <wfelipefarina@gmail.com>
2020-08-13 18:30:32 -0300

    Resolving exercise 5

--- src/createStudent.js
+++ src/createStudent.js
@@ -15,10 +15,10 @@
     estudante.feedback() // Retorna: 'Eita pessoa boa!'
 */
 
-const createStudent = (name) => {
+const createStudent = name => {
   return {
-    name: name,
-    feedback: () => `Eita pessoa boa!`,
+    name,
+    feedback: () => 'Eita pessoa boa!',
   };
 };
 
