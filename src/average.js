781b5b04671770f7edddcbbc8b8199695ef55f67
ilanribeiro <ilanribeiro@gmail.com>
2020-08-13 18:35:18 -0300

    requirement 1 passed all- average.js - adjust condition to verify if array item is not a number

--- src/average.js
+++ src/average.js
@@ -13,16 +13,22 @@
 */
 
 const average = (array) => {
+  // variable to receive sum of all itens in array
   let sumArray = 0;
+  // analyzing if array is empty
   if (array.length === 0) {
     return undefined;
   }
+  // repetition loop to analyze expected conditions or sum array itens
   for (let index = 0; index < array.length; index += 1) {
-    if (typeof (array[index]) === 'string' || array[index] === ' ') {
+    // analyze expected conditions of existence
+    if (typeof (array[index]) !== 'number') {// || array[index] === ' ') {
       return undefined;
     }
+    // sum all itens of array
     sumArray += array[index];
   }
+  // if all conditions ok, return the arithmetic mean of the items in the array
   return Math.round(sumArray / array.length);
 };
 
