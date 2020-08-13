b0cbf020dd4492b8ac0e6aa48e20ecfb3802d778
Igor Leão <igorleao.eng@gmail.com>
2020-08-13 19:28:08 -0300

    vqv tirar a contatenação

--- src/vqv.js
+++ src/vqv.js
@@ -14,12 +14,12 @@
 */
 
 const vqv = (nome, idade) => {
- if (typeof nome === 'string' && typeof idade === 'number') {
-   return 'Oi, meu nome é '+nome+'!\n' +
-        'Tenho '+idade+' anos,\n' +
-        'trabalho na Trybe e mando muito em programação!\n' +
-        '#VQV!'
-   } else return undefined;
+  if (typeof nome === 'string' && typeof idade === 'number') {
+    return `Oi, meu nome é ${nome}!
+    Tenho ${idade} anos,
+    trabalho na Trybe e mando muito em programação!
+    #VQV!`;
+  } return undefined;
 };
 
 
