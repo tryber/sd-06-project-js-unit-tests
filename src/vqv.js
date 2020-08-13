3f75e781883b9a76b7977d1dc3193ff655f4289a
Igor Leão <igorleao.eng@gmail.com>
2020-08-13 20:10:07 -0300

    teste

--- src/vqv.js
+++ src/vqv.js
@@ -16,9 +16,9 @@
 const vqv = (nome, idade) => {
   if (typeof nome === 'string' && typeof idade === 'number') {
     return `Oi, meu nome é ${nome}!
-    Tenho ${idade} anos,
-    trabalho na Trybe e mando muito em programação!
-    #VQV!`;
+Tenho ${idade} anos,
+trabalho na Trybe e mando muito em programação!
+#VQV!`;
   } return undefined;
 };
 
