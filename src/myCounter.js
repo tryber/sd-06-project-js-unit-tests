0aac0285ad3697a98a5d85798342de967937e839
Luciano Scalfone <luciano.scalfone@gmail.com>
2020-08-14 11:28:07 -0300

    issues requisito 8

--- src/myCounter.js
+++ src/myCounter.js
@@ -19,11 +19,11 @@ const myCounter = () => {
   const myArray = [];
   for (let counter = 0; counter <= 3; counter += 1) {
     myArray.push(counter);
-    for (let counter = 2; counter <= 3; counter += 1) {
-      myArray.push(counter);
+    for (let count = 2; count <= 3; count += 1) {
+      myArray.push(count);
     }
   }
   return myArray;
 };
-console.log(myCounter());
+
 module.exports = myCounter;
