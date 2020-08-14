ac48bb975f8aa305336ba8f0da127c9c77fafb37
Tiago Berwanger <berwangertiago@gmail.com>
2020-08-13 21:58:27 -0300

    Requisito 4 - ajustes

--- tests/circle.spec.js
+++ tests/circle.spec.js
@@ -35,9 +35,9 @@ describe('#circle', () => {
     // Teste se a função, quando não recebe nenhum parâmetro, retorna undefined.
     assert.strictEqual(circle(), undefined);
     // Teste que a função retorna, dentro de um objeto, a circunferência correta para um círculo de raio 2.
-    assert.deepStrictEqual({circumference: parseFloat(((circle(2).circumference)).toPrecision(4))}, {circumference: 12.56});
+    assert.deepStrictEqual(parseFloat(((circle(2).circumference)).toPrecision(4)), 12.56);
     // Teste que a função retorna, dentro de um objeto, a área correta para um círculo de raio 3.
-    assert.deepStrictEqual({area: parseFloat(((circle(3).area)).toPrecision(4))}, {area: 28.26});   
+    assert.deepStrictEqual(parseFloat(((circle(3).area)).toPrecision(4)), 28.26);   
     // Teste que a função retorna, num objeto, os dados corretos de um círculo de raio 3.
     assert.deepStrictEqual ({radius: 3, area: parseFloat(((circle(3).area)).toPrecision(4)), circumference: parseFloat(((circle(3).circumference)).toPrecision(4))}, {radius: 3, area: 28.26, circumference: 18.84});
   });
