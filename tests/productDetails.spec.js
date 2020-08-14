8899f2dd58b63dbb6b45736f179ecc718ab5c78c
Macarena Rodriguez <macarenar697@gmail.com>
2020-08-14 16:07:37 -0300

    Requisito 6, corrovorar codeclimate

--- tests/productDetails.spec.js
+++ tests/productDetails.spec.js
@@ -33,12 +33,24 @@ const productDetails = require('../src/productDetails');
 
 describe('#productDetails', () => {
   it('tests the function has the correct behaviour', () => {
-    assert.fail();
+    //  assert.fail();
     // ESCREVA SEUS TESTES ABAIXO:
     // Teste que o retorno da função é um array.
+    //  funcao Array.isArray()
+    //https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Reference/Global_Objects/Array/isArray
+    assert.strictEqual(Array.isArray(productDetails()), true);
     // Teste que o array retornado pela função contém dois itens dentro.
+    assert.strictEqual(productDetails().length, 2);
     // Teste que os dois itens dentro do array retornado pela função são objetos.
+    assert.strictEqual(typeof productDetails()[0], 'object');
+    assert.strictEqual(typeof productDetails()[1], 'object');
     // Teste que os dois objetos são diferentes entre si.
+    assert.strictEqual(productDetails()[0] !== productDetails()[0], true);
     // (Difícil) Teste que os dois productIds terminam com 123.
+    //  funcao substr(): 
+    //  https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Reference/Global_Objects/String/substr
+    assert.strictEqual(productDetails()[0].details.productId.substr(-3), '123');
+    assert.strictEqual(productDetails()[1].details.productId.substr(-3), '123');
+
   });
 });
