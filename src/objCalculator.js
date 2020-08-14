e1810343b49053379534a1d6d02868d5603ffaac
Bruna Franciele <brunafranciele1902@gmail.com>
2020-08-13 22:16:13 -0300

    requisito 6

--- src/objCalculator.js
+++ src/objCalculator.js
@@ -21,16 +21,16 @@
 
 const calculator = {};
 
-calculator.add = (num1, num2)=>{
+calculator.add = (num1, num2) => {
   return num1 + num2;
 };
-calculator.sub = (num1, num2)=>{
+calculator.sub = (num1, num2) => {
   return num1 - num2;
 };
-calculator.mult = (num1, num2)=>{
+calculator.mult = (num1, num2) => {
   return num1 * num2;
 };
-calculator.div = (num1, num2)=>{
+calculator.div = (num1, num2) => {
   return Math.trunc(num1 / num2);
 };
 
