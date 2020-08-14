f12db0088c66b1d8a6f8ad4ce10182db96f61b7c
Flavio Oliveira <flavsoliver@gmail.com>
2020-08-14 17:24:17 -0300

    requisito 04

--- tests/circle.spec.js
+++ tests/circle.spec.js
@@ -25,13 +25,36 @@ const circle = require('../src/circle');
 
 describe('#circle', () => {
   it('given a radius, should return an object with circles info', () => {
-    assert.fail();
+    // assert.fail();
     // ESCREVA SEUS TESTES ABAIXO:
     // Teste se circle retorna um objeto.
+    const circle1 = circle(1);
+    assert.strictEqual(typeof circle1, 'object');
     // Teste se o objeto retornado tem 3 entradas.
+    assert.deepStrictEqual(circle(1), {radius: 1, area: 3.14, circumference: 6.28});
     // Teste se a função, quando não recebe nenhum parâmetro, retorna undefined.
+    assert.deepStrictEqual(circle(), undefined);
     // Teste que a função retorna, dentro de um objeto, a circunferência correta para um círculo de raio 2.
+    const circumferenceObject = circle(2);
+    assert.deepStrictEqual(circumferenceObject.circumference, 12.56);
     // Teste que a função retorna, dentro de um objeto, a área correta para um círculo de raio 3.
+    const areaObject = circle(3);
+    assert.deepStrictEqual(areaObject.area, 28.259999999999998);
     // Teste que a função retorna, num objeto, os dados corretos de um círculo de raio 3.
+    assert.deepStrictEqual(circle(3), {radius: 3, area: 28.259999999999998, circumference: 18.84});
+
+    
+
+
+
+
+    //   vqv('Alberto', 30),
+    //   'Oi, meu nome é Alberto!\n' +
+    //     'Tenho 30 anos,\n' +
+    //     'trabalho na Trybe e mando muito em programação!\n' +
+    //     '#VQV!',
+    // assert.deepStrictEqual(numbers([1, 2, 3, 4, 5]), true);
+
+
   });
 });
