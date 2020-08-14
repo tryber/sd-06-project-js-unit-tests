71ca32a300c75b90552dc102357b0dd4949bd2a0
Lucas Santiago Gomide <lucas.sgomide92@gmail.com>
2020-08-14 18:59:01 -0300

    Adding eighth requirement

--- src/vqv.js
+++ src/vqv.js
@@ -16,6 +16,18 @@
       #VQV!'
 */
 
-const vqv = (nome, idade) => {};
+const vqv = (nome, idade) => {
+  let fullString = (`Oi, meu nome é ${nome}!
+Tenho ${idade} anos,
+trabalho na Trybe e mando muito em programação!
+#VQV!`);
+
+  if (nome === undefined || idade === undefined) {
+    fullString = undefined;
+  }
+  return fullString;
+};
+
+console.log(vqv());
 
 module.exports = vqv;
