e136ed919d81ba1a898ad11fd43bde396c6942b9
higoramorim <1810042@aluno.univesp.br>
2020-08-13 21:27:54 -0300

    average.js test - codeclimate

--- src/average.js
+++ src/average.js
@@ -15,18 +15,17 @@
 const average = (array) => {
   let soma = 0;
   let media = 0;
-  if (array.length === 0)
-    return undefined
-  for (let index = 0 ;index < array.length; index++) {
-    if (typeof array[index] == 'string')
+  if (array.length === 0) {
+    return undefined;
+  }
+  for (let index = 0; index < array.length; index += 1) {
+    if (typeof array[index] === 'string') {
   return undefined;
+    }
   soma += array[index];
   media = soma / array.length;
-  }
+};
   return Math.round(media);
-  }
+}
 
-//font:https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/forEach
-//font:https://pt.stackoverflow.com/questions/13720/como-fa%C3%A7o-para-arredondar-um-valor-de-39-54-para-39/13724
-//font: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/reduce
 module.exports = average;
