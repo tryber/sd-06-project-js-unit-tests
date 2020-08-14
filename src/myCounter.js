e09c932fbfb12f8f59db983dbc99d978e4dbe40d
Igor Leão <igorleao.eng@gmail.com>
2020-08-13 23:17:45 -0300

    erros obj and mycounter

--- src/myCounter.js
+++ src/myCounter.js
@@ -16,7 +16,7 @@
 */
 
 const myCounter = () => {
-  let myArray = [];
+  const myArray = [];
   for (let counter = 0; counter <= 3; counter += 1) {
     myArray.push(counter);
     for (let counter1 = 2; counter1 <= 3; counter1 += 1) {
