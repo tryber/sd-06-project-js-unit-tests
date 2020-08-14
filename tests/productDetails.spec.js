c4f5e0ba5d70be6d5981a201b227777a628f1ae3
Leandro Parisi <lepcbelisario@gmail.com>
2020-08-14 15:38:10 -0300

    testando ajustar productDetails

--- tests/productDetails.spec.js
+++ tests/productDetails.spec.js
@@ -40,11 +40,10 @@ describe('#productDetails', () => {
     // Teste que o array retornado pela função contém dois itens dentro.
     assert.equal(productDetails('tes', 'tes').length, 2, 'array should have 2 items');
     // Teste que os dois itens dentro do array retornado pela função são objetos.
-    for (index = 0; index <= 1; index += 1) {
-      assert.equal(typeof (productDetails('tes', 'tes')[index]), 'object', 'each of returned array indexes should be an object');
-    }
+    assert.equal(typeof (productDetails('tes', 'tes')[0]), 'object', 'each of returned array indexes should be an object');
+    assert.equal(typeof (productDetails('tes', 'tes')[1]), 'object', 'each of returned array indexes should be an object');
     // Teste que os dois objetos são diferentes entre si.
-    assert.notEqual(productDetails('tes', 'tes')[0], productDetails('tes', 'tes')[1], 'Objects should not be equal');
+    assert.notDeepEqual(productDetails('tes', 'test')[0], productDetails('tes', 'test')[1], 'Objects should not be equal');
     // (Difícil) Teste que os dois productIds terminam com 123.
     assert.match(productDetails('tes', 'tes')[0].details.productId, /^.*123$/, 'ProductID should end with 123');
   });
