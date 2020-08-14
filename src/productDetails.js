5c140b13a2d27863a6d481f297c013db77f49884
Leandro Parisi <lepcbelisario@gmail.com>
2020-08-14 15:41:58 -0300

    testando ajustar productDetails 3

--- src/productDetails.js
+++ src/productDetails.js
@@ -40,3 +40,15 @@ const productDetails = (firstProduct, secondProduct) => [
 ];
 
 module.exports = productDetails;
+
+assert = require('assert');
+assert.equal(Array.isArray(productDetails('tes', 'tes')), true, 'Function result should be an Array');
+// Teste que o array retornado pela função contém dois itens dentro.
+assert.equal(productDetails('tes', 'tes').length, 2, 'array should have 2 items');
+// Teste que os dois itens dentro do array retornado pela função são objetos.
+assert.equal(typeof (productDetails('tes', 'tes')[0]), 'object', 'each of returned array indexes should be an object');
+assert.equal(typeof (productDetails('tes', 'tes')[1]), 'object', 'each of returned array indexes should be an object');
+// Teste que os dois objetos são diferentes entre si.
+assert.notDeepEqual(productDetails('tes', 'test')[0], productDetails('tes', 'test')[1], 'Objects should not be equal');
+// (Difícil) Teste que os dois productIds terminam com 123.
+assert.match(productDetails('tes', 'tes')[0].details.productId, /^.*123$/, 'ProductID should end with 123');
