be676b13ba2bf122e414c4e913685b4c5308b378
isabellavjs <isabellavjs@gmail.com>
2020-08-14 09:32:32 -0300

    Requisitos 6

--- tests/productDetails.spec.js
+++ tests/productDetails.spec.js
@@ -44,5 +44,7 @@ describe('#productDetails', () => {
     // Teste que os dois objetos são diferentes entre si.
     assert.ok(productDetails()[0] !== productDetails()[1])
     // (Difícil) Teste que os dois productIds terminam com 123.
+    assert.ok(productDetails()[0].details.productId.slice(-3) === '123')
+    assert.ok(productDetails()[1].details.productId.slice(-3) === '123')
   });
 });
