c7dfa0bd58b85949dd805c380ebac289d3b7b365
Bruna Franciele <brunafranciele1902@gmail.com>
2020-08-13 22:11:21 -0300

    requisito 6

--- src/objCalculator.js
+++ src/objCalculator.js
@@ -13,7 +13,6 @@
   Parâmetros:
   - Um número inteiro;
   - Um número inteiro;
- 
 
   Comportamento:
   calculator.add(1, 1) // Retorno: 2;
@@ -24,13 +23,13 @@ const calculator = {};
 
 calculator.add = (num1, num2) => {
   return num1 + num2;
-},
+}
 calculator.sub = (num1, num2) => {
   return num1 - num2;
-},
+}
 calculator.mult = (num1, num2) => {
   return num1 * num2;
-},
+}
 calculator.div = (num1, num2) => {
   return Math.trunc(num1 / num2);
 };
