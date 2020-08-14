673673c9b2dc8c916b9a5098469dd51cd2c3a094
paulo-lins <paulolins100@gmail.com>
2020-08-14 15:00:34 -0300

    createStudent.js correcao do code climate

--- src/createStudent.js
+++ src/createStudent.js
@@ -15,8 +15,8 @@
     estudante.feedback() // Retorna: 'Eita pessoa boa!
 */
 
-const createStudent = (name) => ({
-  name: name,
+const createStudent = name => ({
+  name,
   feedback: () => 'Eita pessoa boa!',
 });
 
