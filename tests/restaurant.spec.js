63ba1f39244d1fffcf6b0fc9538b51a14c3e66ce
ricardorosa-dev <ricardorosa@gmail.com>
2020-08-14 18:33:26 -0300

    -codeclimate.

--- tests/restaurant.spec.js
+++ tests/restaurant.spec.js
@@ -51,8 +51,9 @@ const createMenu = require('../src/restaurant');
 
 describe('#createMenu', () => {
   it('tests the function has the correct behaviour', () => {
-    assert.fail();
+    //assert.fail();
     // TESTE 1: Verifique que, dado um objeto qualquer passado como um parâmetro para a função createMenu(), checa se o retorno da função é um objeto no seguinte formato: { fetchMenu: objetoQualquer }.
+    assert.deepStrictEqual(createMenu(obj), { fetchMenu: obj });
     // ```
     // createMenu(objetoQualquer) // Retorno: { fetchMenu: objetoQualquer }
     // ```
