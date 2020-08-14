b0525b00c046135e825192895aea0b12ef45b5bd
cyranowebdev <cyrano@cyranowebdev.com>
2020-08-14 11:56:09 -0300

    ajustes code climate requisito 7.

--- src/objCalculator.js
+++ src/objCalculator.js
@@ -20,16 +20,18 @@
 */
 
 const calculator = {
-  add: (num1, num2) => { if (Number.isInteger(num1) && Number.isInteger(num2)) {
-    return num1 + num2;
-  }},
-  mult: (num1, num2) => { if (Number.isInteger(num1) && Number.isInteger(num2)) {
-    return num1 * num2;
-  }},
-  div: (num1, num2) => { if (Number.isInteger(num1) && Number.isInteger(num2)) {
-    return Math.floor(num1 / num2);
-  }},
-  sub: (num1, num2) => { if (Number.isInteger(num1) && Number.isInteger(num2)) return num1 - num2; },
+  add: (num1, num2) => {
+    return Number.isInteger(num1) && Number.isInteger(num2) ? num1 + num2 : undefined;
+  },
+  mult: (num1, num2) => {
+    return Number.isInteger(num1) && Number.isInteger(num2) ? num1 * num2 : undefined;
+  },
+  div: (num1, num2) => {
+    return Number.isInteger(num1) && Number.isInteger(num2) ? Math.floor(num1 / num2) : undefined;
+  },
+  sub: (num1, num2) => {
+    return Number.isInteger(num1) && Number.isInteger(num2) ? num1 - num2 : undefined;
+  },
 };
 
 module.exports = calculator;
