93a5a5455fcc4fc9771eae1d39f957eefd2bc26d
Vinicius Buzato <viniciusbuzato@gmail.com>
2020-08-14 17:13:47 -0300

    Step 5 - Done

--- src/createStudent.js
+++ src/createStudent.js
@@ -15,6 +15,14 @@
     estudante.feedback() // Retorna: 'Eita pessoa boa!'
 */
 
-const createStudent = () => {};
+const createStudent = (nameStudent) => {
+  const obj = {
+    name: nameStudent,
+    feedback() {
+      return 'Eita pessoa boa!';
+    },
+  };
+  return obj;
+};
 
 module.exports = createStudent;
