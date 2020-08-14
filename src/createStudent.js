9f53a32de3b82fc40899bcf6ddab091b0906d413
kissyla <kissylamottapereira@gmail.com>
2020-08-14 16:55:20 -0300

    req 5: função student

--- src/createStudent.js
+++ src/createStudent.js
@@ -15,8 +15,12 @@
     estudante.feedback() // Retorna: 'Eita pessoa boa!'
 */
 
-const createStudent = (name) => {
-  const estudante = createStudent('Leandrão, o Lobo Solitário')
+const createStudent = (nome) => {
+  const estudante = {
+    name: nome,
+    feedback: () => 'Eita pessoa boa!',
+  };
+  return estudante;
 };
 
 module.exports = createStudent;
