c8481204a0540f8394b032de859e76f244ba1bcb
Arthur de Sa <arthurdesa94@gmail.com>
2020-08-14 19:32:19 -0300

    aleluia o primeiro foi

--- src/average.js
+++ src/average.js
@@ -12,8 +12,21 @@
     - average([1, '2']) // Retorno: undefined;
 */
 
-const average = () => {
-  //add your implementation here
+const average = (array) => {
+let total = 0;
+  if (array.length === 0) {
+    return undefined;
+  }
+  for (let index = 0; index < array.length; index +=1) {
+    if (typeof array[index] === 'number') {
+      total += array[index];
+    } 
+    else {
+      return undefined;
+    }
+  }
+  let avg = Math.round(total / array.length);
+  return avg;
 };
 
 module.exports = average;
