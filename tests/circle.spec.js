272806e1344ee46d35993febe6a880602da53ff7
Daniel Jesus <dev.danieljs@gmail.com>
2020-08-14 15:24:40 -0300

    Doing tests to validate function circle with Radius, Area and Circumference

--- tests/circle.spec.js
+++ tests/circle.spec.js
@@ -25,13 +25,25 @@ const circle = require('../src/circle');
 
 describe('#circle', () => {
   it('given a radius, should return an object with circles info', () => {
-    assert.fail();
+    // assert.fail();
     // ESCREVA SEUS TESTES ABAIXO:
+
     // Teste se circle retorna um objeto.
+    assert.strictEqual(typeof circle(2), 'object');
+
     // Teste se o objeto retornado tem 3 entradas.
+    assert(circle(1), '{ radius: 1, area: 3.14, circumference: 6.28 }');
+
     // Teste se a função, quando não recebe nenhum parâmetro, retorna undefined.
+    assert.strictEqual(circle(), undefined);
+
     // Teste que a função retorna, dentro de um objeto, a circunferência correta para um círculo de raio 2.
+    assert(circle(2), '{ radius: 2, area: 12.56, circumference: 12.56 }');
+
     // Teste que a função retorna, dentro de um objeto, a área correta para um círculo de raio 3.
+    assert(parseFloat(circle(3)).toPrecision(2), '{radius: 3, area: 28,26, circumference: 18.84}');
+
     // Teste que a função retorna, num objeto, os dados corretos de um círculo de raio 3.
+    assert(circle(2), '{ radius: 2, area: 12.56, circumference: 12.56 }');
   });
 });
