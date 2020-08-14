d5f68f585b247c8c0b20a03ef4671cbd7df9b5bf
Fabio Camargos <fabiocmmcm@gmail.com>
2020-08-14 19:54:41 -0300

    o do student foi facil grazadeus

--- src/createStudent.js
+++ src/createStudent.js
@@ -15,6 +15,12 @@
     estudante.feedback() // Retorna: 'Eita pessoa boa!'
 */
 
-const createStudent = () => {};
+const createStudent = (anyName) => {
+  var objeto = {
+    name: anyName,
+    feedback: () => "Eita pessoa boa!"
+  }
+  return objeto;
+};
 
 module.exports = createStudent;
