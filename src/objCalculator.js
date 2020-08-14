2fbd45a7e7013924b5888d92357ba4835a12e31e
Macarena Rodriguez <macarenar697@gmail.com>
2020-08-14 16:49:10 -0300

    Requisito 7, corrovorar codeclimate

--- src/objCalculator.js
+++ src/objCalculator.js
@@ -19,6 +19,13 @@
   calculator.div(3, 2) // Retorno: 1;
 */
 
-const calculator = {};
+const calculator = {
+  add: add = (num1, num2) => {return num1 + num2},
+  mult: mult = (num1, num2) => {return num1 * num2},
+  //  Math.round asia baixo
+  //  https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Reference/Global_Objects/Math/round
+  div: div = (num1, num2) => {return Math.round(num1 / num2 - 0.1)},
+  sub: sub = (num1, num2) => {return num1 - num2},
+};
 
 module.exports = calculator;
