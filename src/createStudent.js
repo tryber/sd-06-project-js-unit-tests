6270f5249e91b9caf8009af39e6db85aac40b5e1
kissyla <kissylamottapereira@gmail.com>
2020-08-14 16:33:16 -0300

    requisito 4: teste função circle

--- src/createStudent.js
+++ src/createStudent.js
@@ -15,6 +15,8 @@
     estudante.feedback() // Retorna: 'Eita pessoa boa!'
 */
 
-const createStudent = () => {};
+const createStudent = (name) => {
+  const estudante = createStudent('Leandrão, o Lobo Solitário')
+};
 
 module.exports = createStudent;
