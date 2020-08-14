de82f55febbd04f65560a929fbee7732d4f6bb19
Cássio Rubens <cassiorubensadv@gmail.com>
2020-08-14 19:04:01 -0300

    Implementando função do arquivo src/createStudent.js

--- src/createStudent.js
+++ src/createStudent.js
@@ -15,6 +15,12 @@
     estudante.feedback() // Retorna: 'Eita pessoa boa!'
 */
 
-const createStudent = () => {};
+const createStudent = (name) => {
+  let studentName = {
+    name,
+    feedback: () => 'Eita pessoa boa!',
+  }
+  return studentName;
+};
 
 module.exports = createStudent;
