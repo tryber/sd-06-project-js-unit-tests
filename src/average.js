77e88f1579fc48df58a497f07968a6f40db9cacd
Matheus Dias <matheuscara2000@gmail.com>
2020-08-14 16:37:49 -0300

    code-climate

--- src/average.js
+++ src/average.js
@@ -17,15 +17,13 @@ const average = (array) => {
   if (array.length === 0) {
     return undefined;
   }
-  for (let i = 0; i < array.length; i++) {
+  for (let i = 0; i < array.length; i += 1) {
     if (typeof array[i] !== 'number') {
       return undefined;
     } averageSum += Math.round(array[i]);
   }
-  let result = Math.round(averageSum/array.length);
+  const result = Math.round(averageSum / array.length);
   return result;
 };
-
-average([3,"4",4])
 
 module.exports = average;
