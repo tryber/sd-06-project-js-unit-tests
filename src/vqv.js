1463128caffdb7df113d460ccd8fee59406b8c2e
Keila Sales <k.f.saless@gmail.com>
2020-08-14 16:29:26 -0300

    Requisito 3

--- src/vqv.js
+++ src/vqv.js
@@ -16,6 +16,18 @@
       #VQV!'
 */
 
-const vqv = (nome, idade) => {};
+const vqv = (nome, idade) => {
+  let frase = `Oi, meu nome é ${nome}!
+Tenho ${idade} anos,
+trabalho na Trybe e mando muito em programação!
+#VQV!`
+
+  if ( nome === undefined && idade === undefined) {
+    return undefined;
+  }
+  return frase;
+}
+
+console.log(vqv('Tunico', 30));
 
 module.exports = vqv;
