dd212f6ba58cc12bfeef6fbb44c7a4e9fb28d0d6
VinicinBraga <vinicius@bragamatta.com.br>
2020-08-14 11:24:19 -0300

    Refatorando o myCounter

--- src/myCounter.js
+++ src/myCounter.js
@@ -17,13 +17,15 @@
 
 const myCounter = () => {
   var myArray = [];
-  for (var counter = 0; counter <= 3; counter += 1) {
-    myArray.push(counter);
-    for (var counter = 2; counter <= 3; counter += 1) {
-      myArray.push(counter);
+  for (var firstCounter = 0; firstCounter <= 3; firstCounter += 1) {
+    myArray.push(firstCounter);
+    for (var secondCounter = 2; secondCounter <= 3; secondCounter += 1) {
+      myArray.push(secondCounter);
     }
   }
   return myArray;
 };
 
 module.exports = myCounter;
+
+console.log(myCounter());
