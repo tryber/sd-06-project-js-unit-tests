859f3199f4223f208ebb3935d48b061f6c5c875e
AlexanderFeitosa <alexander.feitosa@gmail.com>
2020-08-14 00:17:23 -0300

    Project JS Unit Tests : Seventh Task Fixes

--- src/objCalculator.js
+++ src/objCalculator.js
@@ -24,7 +24,7 @@ const calculator = {
   mult: (num1, num2) => num1 * num2,
   div: (num1, num2) => {
     if (num2 !== 0) {
-      return num1 / num2;
+      return Math.floor(num1 / num2);
     }
     return undefined;
   },
