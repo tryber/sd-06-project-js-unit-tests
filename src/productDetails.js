f0461ef7598ce2ad893c946999904d322295b2f4
Anderson <andersoni.trybe@gmail.com>
2020-08-14 18:56:49 -0300

    productDetails review

--- src/productDetails.js
+++ src/productDetails.js
@@ -40,5 +40,3 @@ const productDetails = (firstProduct, secondProduct) => [
 ];
 
 module.exports = productDetails;
-
-//console.log(productDetails('caminhao', 'ervilha')[1].details.productId.slice(-3));
