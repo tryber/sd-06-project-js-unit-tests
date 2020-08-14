e8189070e507c77228b9ce79db786dd836f09935
isabellavjs <isabellavjs@gmail.com>
2020-08-14 08:18:12 -0300

    Codeclimate

--- src/createStudent.js
+++ src/createStudent.js
@@ -16,8 +16,8 @@
 */
 
 const createStudent = name => ({
-    name,
-    feedback: () => 'Eita pessoa boa!',
+  name,
+  feedback: () => 'Eita pessoa boa!',
 });
 console.log(createStudent('Isabella'));
 
