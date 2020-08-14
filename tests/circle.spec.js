9743df91415b680a799605ab0fbcb6fd2e8c8fd1
Igor Leão <igorleao.eng@gmail.com>
2020-08-13 22:28:31 -0300

    productDetails

--- tests/circle.spec.js
+++ tests/circle.spec.js
@@ -30,7 +30,7 @@ describe('#circle', () => {
     // Teste se circle retorna um objeto.
     assert.deepStrictEqual(typeof(circle(1)), 'object');
     // Teste se o objeto retornado tem 3 entradas.
-    assert.deepEqual(circle(1),  { radius: 1, area: 3.14, circumference: 6.28 })
+    assert.strictEqual(Object.keys(circle(4)).length, 3);
     // Teste se a função, quando não recebe nenhum parâmetro, retorna undefined.
     assert.deepEqual(circle(), undefined)
     // Teste que a função retorna, dentro de um objeto, a circunferência correta para um círculo de raio 2.
--- tests/productDetails.spec.js
+++ tests/productDetails.spec.js
@@ -33,12 +33,22 @@ const productDetails = require('../src/productDetails');
 
 describe('#productDetails', () => {
   it('tests the function has the correct behaviour', () => {
-    assert.fail();
+    // assert.fail();
     // ESCREVA SEUS TESTES ABAIXO:
     // Teste que o retorno da função é um array.
+    assert.strictEqual(Array.isArray(productDetails('array1', 'array2')), true);
     // Teste que o array retornado pela função contém dois itens dentro.
+    assert.strictEqual(productDetails('array1', 'array2').length, 2);
     // Teste que os dois itens dentro do array retornado pela função são objetos.
+    assert.strictEqual(typeof(productDetails('array1', 'array2')), 'object');
     // Teste que os dois objetos são diferentes entre si.
+    const aux = productDetails('array1', 'array2');
+    assert.notDeepStrictEqual(aux[0], aux[1]);
     // (Difícil) Teste que os dois productIds terminam com 123.
+    assert.deepStrictEqual(aux[0].details.productId.slice(-3), '123');
+    assert.deepStrictEqual(aux[1].details.productId.slice(-3), '123');
+
+    // slice e os details.productId https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Reference/Global_Objects/Object/is
+    //https://www.w3schools.com/nodejs/met_assert_notdeepstrictequal.asp
   });
 });
