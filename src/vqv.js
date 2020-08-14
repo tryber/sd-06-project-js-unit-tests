69e1eed1a3f76fd0fd46ad55d3f179fee526668a
Anderson <andersoni.trybe@gmail.com>
2020-08-14 19:29:08 -0300

    String concatenation review

--- src/vqv.js
+++ src/vqv.js
@@ -18,9 +18,9 @@
 
 const vqv = (nome, idade) => {
   if (nome === undefined && idade === undefined) { return undefined; }
-  return`Oi, meu nome é ${nome}!\n` +
+  return `Oi, meu nome é ${nome}!\n` +
   `Tenho ${idade} anos,\n` +
-  'trabalho na Trybe e mando muito em programação!\n' + '#VQV!';
+  'trabalho na Trybe e mando muito em programação!\n#VQV!';
 };
 
 module.exports = vqv;
