ed94ef7fc57ff481f3ed03232fa862536eb0606f
Bruna Franciele <brunafranciele1902@gmail.com>
2020-08-13 23:41:10 -0300

    correção do productDetails

--- src/myCounter.js
+++ src/myCounter.js
@@ -15,21 +15,21 @@
   myCounter() // Retorna: [0, 2, 3, 1, 2, 3, 2, 2, 3, 3, 2, 3];
 */
 
-const myCounter = () => {
-  var myArray = [];
-  for (var counter = 0; counter <= 3; counter += 1) {
-    myArray.push(counter);
-    for (var counter = 2; counter <= 3; counter += 1) {
-      myArray.push(counter);
-      for (var counter = 3; counter <= 3; counter += 1) {
-        myArray.push(counter);
-      for (var counter = 1; counter <= 3; counter += 1) {
-        myArray.push(counter);
-      }
-    }
-  }
-}
-  return myArray;
-};
-console.log(myCounter())
-module.exports = myCounter;
+// const myCounter = () => {
+//   var myArray = [];
+//   for (var counter = 0; counter <= 3; counter += 1) {
+//     myArray.push(counter);
+//     for (var counter = 2; counter <= 3; counter += 1) {
+//       myArray.push(counter);
+//       for (var counter = 3; counter <= 3; counter += 1) {
+//         myArray.push(counter);
+//       for (var counter = 1; counter <= 3; counter += 1) {
+//         myArray.push(counter);
+//       }
+//     }
+//   }
+// }
+//   return myArray;
+// };
+// console.log(myCounter())
+// module.exports = myCounter;
