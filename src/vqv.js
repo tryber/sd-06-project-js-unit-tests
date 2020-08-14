da85f2100346082664e516fa869af07ebf07d5be
andersonmalves <contatodeanderson@gmail.com>
2020-08-14 08:47:10 -0700

    Requisito 3

--- src/vqv.js
+++ src/vqv.js
@@ -16,6 +16,17 @@
       #VQV!'
 */
 
-const vqv = (nome, idade) => {};
+const vqv = (nome, idade) => {
+  if (typeof nome !== 'undefined' && typeof idade !== 'undefined') {
+    return `Oi, meu nome é ${nome}!
+    Tenho ${idade} anos,
+    trabalho na Trybe e mando muito em programação!
+    #VQV!`.replace(/^ {4}/gm, '');
+  } else {
+    return undefined;
+  }
+};
+
+// console.log(vqv('Tunico', 30));
 
 module.exports = vqv;
