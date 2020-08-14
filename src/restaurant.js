417a149c61dc83af4c489834282f3874317429fd
luiz <phellipe501@gmail.com>
2020-08-14 19:10:00 -0300

    fixing issues finall

--- src/restaurant.js
+++ src/restaurant.js
@@ -82,15 +82,15 @@ const createMenu = (obj) => {
     else if (objT.fetchMenu.drink[item] === undefined) objT.fetchMenu.drink[item] = 0;
   };
   objT.pay = () => {
-      let somaF = 0;
-      let somaD = 0;
-      for (let index = 0; index < objT.consumption.length; index += 1) {
+    let somaF = 0;
+    let somaD = 0;
+    for (let index = 0; index < objT.consumption.length; index += 1) {
         const item = objT.consumption[index];
         compare(item);
         somaF += objT.fetchMenu.food[item];
         somaD += objT.fetchMenu.drink[item];
-      }
-      return somaF + somaD;
+    }
+    return somaF + somaD;
   };
 
   return objT;
