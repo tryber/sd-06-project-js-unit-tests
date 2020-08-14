77e77dae9d2ac812308cfe98dcb8684633498a9f
paulo-lins <paulolins100@gmail.com>
2020-08-14 11:41:26 -0300

    corrigido o erro na string

--- src/createStudent.js
+++ src/createStudent.js
@@ -15,6 +15,11 @@
     estudante.feedback() // Retorna: 'Eita pessoa boa!'
 */
 
-const createStudent = () => {};
-
+const createStudent = (name) => ({
+    name,
+    feedback: () => 'Eita pessoa boa!',
+  });
+  // const estudante = createStudent('Leandrão, o Lobo Solitário');
+  // console.log(estudante.feedback());
 module.exports = createStudent;
+
