5fc476c2992b51d6542bbc61100377de3b17ab27
Anderson R <misterregisslv@gmail.com>
2020-08-14 15:40:00 -0300

    tarefa unit test productDetails concluída

--- tests/productDetails.spec.js
+++ tests/productDetails.spec.js
@@ -33,12 +33,17 @@ const productDetails = require('../src/productDetails');
 
 describe('#productDetails', () => {
   it('tests the function has the correct behaviour', () => {
-    assert.fail();
     // ESCREVA SEUS TESTES ABAIXO:
     // Teste que o retorno da função é um array.
+    assert.deepStrictEqual(productDetails('1','2'),[{"details": {"productId": "1123"}, "name": "1"}, {"details": {"productId": "2123"}, "name": "2"}]);
     // Teste que o array retornado pela função contém dois itens dentro.
+    assert.deepStrictEqual(productDetails('1','2'), productDetails('1','2'));
     // Teste que os dois itens dentro do array retornado pela função são objetos.
+    assert.deepStrictEqual(typeof(productDetails('1','2')), 'object');
     // Teste que os dois objetos são diferentes entre si.
+    assert.notDeepStrictEqual(productDetails('1','2'), ('1','2'));
     // (Difícil) Teste que os dois productIds terminam com 123.
+    assert.deepStrictEqual(productDetails('1','2')[0]["details"]["productId"], '1123');
+    assert.deepStrictEqual(productDetails('1','2')[1]['details']['productId'], '2123');
   });
 });
