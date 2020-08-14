3f471c6ffb8c099dd1b26773f0dfdd8aaebc42cd
us19861229c <rcks.santos@gmail.com>
2020-08-14 16:17:04 -0300

    Testa a função circle.js de acordo com os requisitos da l32

--- tests/circle.spec.js
+++ tests/circle.spec.js
@@ -30,6 +30,7 @@ describe('#circle', () => {
     // Teste se circle retorna um objeto.
     assert.strictEqual(typeof circle(1), 'object');
     // Teste se o objeto retornado tem 3 entradas.
+    assert.deepStrictEqual(circle(7),{ radius: 7, area: 153.86, circumference: 43.96 } );
     // Teste se a função, quando não recebe nenhum parâmetro, retorna undefined.
     // Teste que a função retorna, dentro de um objeto, a circunferência correta para um círculo de raio 2.
     // Teste que a função retorna, dentro de um objeto, a área correta para um círculo de raio 3.
