a2dcbf9ffa0e316e5f6a629f505abd2df741714a
higoramorim <1810042@aluno.univesp.br>
2020-08-14 21:45:30 -0300

    createStudent.spec.js

--- src/createStudent.js
+++ src/createStudent.js
@@ -15,6 +15,12 @@
     estudante.feedback() // Retorna: 'Eita pessoa boa!'
 */
 
-const createStudent = () => {};
+const createStudent = (person) => {
+  let obj = {
+    name: person,
+    feedback: () => 'Eita pessoa boa!',
+  };
+  return obj
+};
 
 module.exports = createStudent;
