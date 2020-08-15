56dce6d8081c5178709d1a40bdf36194830ebd60
Paulo Afonso Volpin <paulo.volpin@gmail.com>
2020-08-15 08:22:35 -0300

    Corrections based on Codeclimate

--- src/vqv.js
+++ src/vqv.js
@@ -16,9 +16,12 @@
       #VQV!'
 */
 
-const vqv = (nome, idade) => { return (nome && idade) ? `Oi, meu nome é ${nome}!
-Tenho ${idade} anos,
-trabalho na Trybe e mando muito em programação!
-#VQV!` : undefined; };
+const vqv = (nome, idade) => {
+  const msg (nome && idade) ? `Oi, meu nome é ${nome}!
+  Tenho ${idade} anos,
+  trabalho na Trybe e mando muito em programação!
+  #VQV!` : undefined;
+  return msg;
+};
 
 module.exports = vqv;
