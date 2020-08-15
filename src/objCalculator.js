4477ecc75172a888932e08bd74257ab4c0c42341
Flavio Oliveira <flavsoliver@gmail.com>
2020-08-15 06:18:20 -0300

    requisito 07

--- src/objCalculator.js
+++ src/objCalculator.js
@@ -19,6 +19,20 @@
   calculator.div(3, 2) // Retorno: 1;
 */
 
-const calculator = {};
+const calculator = {
+  number1: 0,
+  number2: 0,
 
+  add: (number1, number2) => number1 + number2,
+  mult: (number1, number2) => number1 * number2,
+  div: (number1, number2) => parseInt(number1 / number2, 10),
+  sub: (number1, number2) => number1 - number2,
+
+};
+
+
+console.log(calculator.add(1, 3));
+console.log(calculator.mult(10, 3));
+console.log(calculator.div(5, 2));
+console.log(calculator.sub(1, 3));
 module.exports = calculator;
