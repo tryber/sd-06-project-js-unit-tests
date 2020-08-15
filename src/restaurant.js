9bfcb359bc4329084bb25247419e03304ae0e645
Tiago Berwanger <berwangertiago@gmail.com>
2020-08-14 23:02:25 -0300

    Requisito 9 - parte I

--- src/restaurant.js
+++ src/restaurant.js
@@ -71,6 +71,38 @@
 
 // PASSO 4: Adicione ao objeto retornado por `createMenu()` uma chave `pay` com uma função que varre todo os itens de `objetoRetornado.consumption`, soma o preço de todos checando-os no menu e retorna o valor somado acrescido de 10%. DICA: para isso, você precisará varrer tanto o objeto da chave `food` quanto o objeto da chave `drink`.
 
-const createMenu = () => {};
+const createMenu = (food, drink ) => ({ fetchMenu: {food, drink} });
+
+console.log(createMenu({'coxinha': 3.90, 'sanduiche': 9.90}, {'agua': 3.9, 'cerveja': 6.9}))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 module.exports = createMenu;
--- tests/restaurant.spec.js
+++ tests/restaurant.spec.js
@@ -51,12 +51,15 @@ const createMenu = require('../src/restaurant');
 
 describe('#createMenu', () => {
   it('tests the function has the correct behaviour', () => {
-    assert.fail();
+    // assert.fail();
     // TESTE 1: Verifique que, dado um objeto qualquer passado como um parâmetro para a função createMenu(), checa se o retorno da função é um objeto no seguinte formato: { fetchMenu: objetoQualquer }.
     // ```
     // createMenu(objetoQualquer) // Retorno: { fetchMenu: objetoQualquer }
     // ```
     // Agora faça o PASSO 1 no arquivo `src/restaurant.js`.
+
+    assert.deepEqual(createMenu({'coxinha': 3.90, 'sanduiche': 9.90}, {'agua': 3.9, 'cerveja': 6.9}), {fetchMenu: {food: {'coxinha': 3.90, 'sanduiche': 9.90}, drink: {'agua': 3.9, 'cerveja': 6.9 }}});
+
     // --------------------------------------------------------------------------------------
     // TESTE 2: Verifique que, dado que a função createMenu foi chamada com o objeto: `{ food: {}, drink: {} }`, verifique que 'objetoRetornado.fetchMenu' retorna um objeto cujas chaves são somente `food` e `drink`.
     // ```
