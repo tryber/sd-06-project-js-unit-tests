9dc9efdebd19a746556d33a7b78071875686dd41
higoramorim <1810042@aluno.univesp.br>
2020-08-15 18:52:29 -0300

    vqv.spec.js

--- src/vqv.js
+++ src/vqv.js
@@ -17,14 +17,13 @@
 */
 
 const vqv = (nome, idade) => {
-  if (nome === '' && idade === '') {
+  if (typeof nome === 'undefined' && typeof idade === 'undefined') {
     return undefined;
   }
-  return `
-    Oi, meu nome é ${nome}!
-  Tenho ${idade} anos,
-  trabalho na Trybe e mando muito em programação!
-  #VQV!`;
-};
+  return `Oi, meu nome é ${nome}!
+Tenho ${idade} anos,
+trabalho na Trybe e mando muito em programação!
+#VQV!`;
+  }; 
 
 module.exports = vqv;
