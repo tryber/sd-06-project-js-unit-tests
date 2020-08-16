5c64f47f903116e61005daa8a7ed7bbdd0b35dd0
ilanribeiro <ilanribeiro@gmail.com>
2020-08-16 07:24:45 -0300

    requirement 3 completed - [vqv.js]

--- src/vqv.js
+++ src/vqv.js
@@ -16,6 +16,18 @@
       #VQV!'
 */
 
-const vqv = (nome, idade) => {};
+const vqv = (nome, idade) => {
+  if (nome === undefined && idade === undefined) {
+    return undefined;
+  };
+
+  let paragraph =
+  `Oi, meu nome é ${nome}!
+Tenho ${idade} anos,
+trabalho na Trybe e mando muito em programação!
+#VQV!`;
+
+  return paragraph;
+};
 
 module.exports = vqv;
