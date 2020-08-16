10427164716226c7c11eb2b229df1f5efb6108a2
LeandroGnu <leandro.feitoza@gmail.com>
2020-08-16 15:42:05 -0300

    Corrigido dois issues do coceclimate

--- src/myCounter.js
+++ src/myCounter.js
@@ -19,14 +19,11 @@ const myCounter = () => {
   var myArray = [];
   for (let counter = 0; counter <= 3; counter += 1) {
     myArray.push(counter);
-    for (let counter = 2; counter <= 3; counter += 1) {
+    for (counter = 2; counter <= 3; counter += 1) {
       myArray.push(counter);
     }
-    //counter += 1;
   }
   return myArray;
 };
-
-console.log(myCounter())
 
 module.exports = myCounter;
