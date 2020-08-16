2df12c91de12240d930c288a2ff34f47f734e93b
flaviolunaferreira <flaviolunaferreira@gmail.com>
2020-08-16 17:45:36 -0300

    manda colocar, agora manda tirar (mais uma vez)

--- src/createStudent.js
+++ src/createStudent.js
@@ -18,7 +18,9 @@
 const createStudent = (nome) => {
   const estudante = {
     name: nome,
-    feedback: () => {'Eita pessoa boa!'}
+    feedback: () => {
+      'Eita pessoa boa!';
+    }
   };
   return estudante;
 };
