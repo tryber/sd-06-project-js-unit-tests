90d15f806ab368563d83b85768ad5039a75a2977
Matheus Dias <matheuscara2000@gmail.com>
2020-08-16 16:42:50 -0300

    Code-climate

--- src/vqv.js
+++ src/vqv.js
@@ -17,8 +17,8 @@
 */
 
 const vqv = (nome, idade) => {
-  if(nome === undefined || idade === undefined )return undefined;
-  return String(`Oi, meu nome é ${nome}!\n`+`Tenho ${idade} anos,\n`+`trabalho na Trybe e mando muito em programação!\n`+`#VQV!`);
+  if (nome === undefined || idade === undefined) return undefined;
+  return String (`Oi, meu nome é ${nome}!\n` + `Tenho ${idade} anos,\n` + `trabalho na Trybe e mando muito em programação!\n` + `#VQV!`);
 };
 
 module.exports = vqv;
