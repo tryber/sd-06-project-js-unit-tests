f77a38ec35d58ce9d7d0b5adfe36414181190514
flaviolunaferreira <flaviolunaferreira@gmail.com>
2020-08-16 18:56:55 -0300

    resolvido a mudanca na variavel counter no myCounter

--- src/myCounter.js
+++ src/myCounter.js
@@ -16,7 +16,7 @@
 */
 
 const myCounter = () => {
-  let myArray = [];
+  const myArray = [];
   for (var counter = 0; counter <= 3; counter += 1) {
     myArray.push(counter);
     for (var count = 2; count <= 3; count += 1) {
