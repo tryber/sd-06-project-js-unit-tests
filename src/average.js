d9410c83eaddfed022f7301433b1cebe9153ea03
rafMewtwo <ponciano.raf@gmail.com>
2020-08-15 22:53:34 -0300

    n confio no cc

--- src/average.js
+++ src/average.js
@@ -17,22 +17,21 @@ const average = (array) => {
   // tst para saber se esta vazio
   if (array.length === 0) {
     return undefined;
-  } else {
+  }
   // for para caminhar na array
     for (let i = 0; i < array.length; i += 1) {
     // teste para encontrar um valor nao numerico
       if (typeof array[i] !== 'number') {
         return undefined;
-      } else {
+      }
       // acumular tudo em soma
         soma += array[i];
-      }
+      
     }
   // tira a media
     soma /= array.length;
   // passa p/ int
-    return Math.round(soma);
-  }
+    return Math.round(soma); 
 };
 console.log(average([-3, -3, -4, -5, -4]));
 
