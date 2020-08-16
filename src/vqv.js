b58bbadf83841b185fa9d29cd3c77758ff8870a8
rafMewtwo <ponciano.raf@gmail.com>
2020-08-15 23:04:50 -0300

    agora vai

--- src/vqv.js
+++ src/vqv.js
@@ -19,13 +19,9 @@
 const vqv = (nome, idade) => {
   if (nome === undefined || idade === undefined) {
     return undefined;
-  } else {
-    const string = 'Oi, meu nome é ' + nome + '!\n' +
-  'Tenho ' + idade + ' anos,\n' +
-  'trabalho na Trybe e mando muito em programação!\n' +
-  '#VQV!';
-    return string;
   }
+    const string = `Oi, meu nome é ${nome}!\nTenho ${idade} anos,\ntrabalho na Trybe e mando muito em programação!\n#VQV!`;
+    return string;
 };
 
 console.log(vqv('Tunico', 29));
