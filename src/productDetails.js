47bdb5f6f4f9fa4c13545896ade2d2145cd013fd
DekoPossas <andrepossaster@gmail.com>
2020-08-17 11:05:15 -0300

    i implemented assert mode of function

--- src/productDetails.js
+++ src/productDetails.js
@@ -24,6 +24,8 @@
   ]
 */
 
+const assert = require('assert');
+
 const productDetails = (firstProduct, secondProduct) => [
   {
     name: firstProduct,
@@ -38,5 +40,19 @@ const productDetails = (firstProduct, secondProduct) => [
     },
   },
 ];
+
+const itens = productDetails('alcool', 'mascara');
+
+// assert.strictEqual(typeof itens[1], 'object');
+// assert.strictEqual(typeof itens[0], 'object');
+// assert.deepEqual((itens[0] !== itens[1]), true);
+
+const idProduto1 = itens[0].details.productId;
+const idProduto2 = itens[1].details.productId;
+const finalString1 = idProduto1.substring(idProduto1.length-3, idProduto1.length);
+const finalString2 = idProduto2.substring(idProduto2.length-3, idProduto2.length);
+assert.strictEqual(finalString1, '123');
+assert.strictEqual(finalString2, '123');
+
 
 module.exports = productDetails;
