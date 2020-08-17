9142e65eab90bb7f5078ba0949e7ce9cc12e3833
Alvaro Neto <alvaroshn@gmail.com>
2020-08-17 15:23:06 -0300

    Restaurant.js

--- src/restaurant.js
+++ src/restaurant.js
@@ -9,20 +9,20 @@ const createMenu = (meuMenu) => {
       let soma = 0;
       const menuSize = Object.keys(restaurant.fetchMenu().drink).length + Object.keys(restaurant.fetchMenu().food).length;
       for (let index = 0; index < menuSize; index += 1) {
-        for (let index2 = 0; index2 < restaurant.consumption.length; index2 += 1) {
-          if (restaurant.consumption[index2] === Object.keys(restaurant.fetchMenu().food)[index]) {
-            soma += Object.values(restaurant.fetchMenu().food)[index];
+        restaurant.consumption.forEach((element) => {
+          if (element === Object.keys(restaurant.fetchMenu().food)[index]) {
+            soma += parseFloat(Object.values(restaurant.fetchMenu().food)[index]);
           }
-          if (restaurant.consumption[index2] === Object.keys(restaurant.fetchMenu().drink)[index]) {
-            soma += Object.values(restaurant.fetchMenu().drink)[index];
+          if (element === Object.keys(restaurant.fetchMenu().drink)[index]) {
+            soma += parseFloat(Object.values(restaurant.fetchMenu().drink)[index]);
           }
-        }
+        });
       }
       return soma * 1.1;
     },
-  };
+  }
   return restaurant;
-};
+}
 
 const objetoInicial = { food: { coxinha: 3.9, sopa: 9.9 }, drink: { agua: 3.9, cerveja: 6.9 } };
 const meuRestaurante = createMenu(objetoInicial);
