59975076dbef44cfc1263acced0b3df5efa32191
Pedro <pedroedu77@gmail.com>
2020-08-17 15:07:41 -0300

    Requisito 3 corrigido

--- src/productDetails.js
+++ src/productDetails.js
@@ -1,4 +1,4 @@
-/*
+ /*
   Dadas duas strings que representam nomes de produtos,
   retorne um array contendo dois objetos com os detalhes dos respectivos produtos.
 
--- src/vqv.js
+++ src/vqv.js
@@ -18,9 +18,12 @@
 
 const vqv = (nome, idade) => {
   if (!nome || !idade) {
-    return undefined
+    return undefined;
   }
-  return `Oi, meu nome é ${nome}!\n` + `Tenho ${idade} anos,\n` + `trabalho na Trybe e mando muito em programação!\n` + `#VQV!`;
+  return `Oi, meu nome é ${nome}!
+Tenho ${idade} anos,
+trabalho na Trybe e mando muito em programação!
+#VQV!`;
 };
 
 module.exports = vqv;
