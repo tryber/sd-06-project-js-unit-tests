4f7233f99862588befc9a3ba4fe341023948f994
Alvaro Neto <alvaroshn@gmail.com>
2020-08-17 15:33:33 -0300

    Restaurant.js

--- src/restaurant.js
+++ src/restaurant.js
@@ -22,7 +22,7 @@ const createMenu = (meuMenu) => {
     },
   };
   return restaurant;
-}
+};
 
 const objetoInicial = { food: { coxinha: 3.9, sopa: 9.9 }, drink: { agua: 3.9, cerveja: 6.9 } };
 const meuRestaurante = createMenu(objetoInicial);
