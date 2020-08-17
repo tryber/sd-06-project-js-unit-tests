089bc1c59470b31a7440679a010968df182252d2
Elias Carvalho <eliascarvalho01@hotmail.com>
2020-08-17 12:59:15 -0300

    Fixing CFC

--- src/restaurant.js
+++ src/restaurant.js
@@ -1,8 +1,6 @@
 /* eslint-disable max-len */
 
 /*
-  Você é responsável por escrever o código do sistema de pedidos de um restaurante. Deve ser possível, através desse sistema, 
-  cadastrar um menu. Dado que um menu foi cadastrado, o sistema deve disponibilizar um objeto através do qual se consegue:
   - ler o menu cadastrado;
   - fazer pedidos;
   - verificar o que foi pedido;
@@ -75,7 +73,7 @@ const createMenu = (menu) => {
   const IDONTKNOW = {
     fetchMenu: () => menu,
     consumption: [],
-    order: (...string) => IDONTKNOW.consumption = string,
+    order: function(...string) { IDONTKNOW.consumption = string },
     pay: () => {
       const obj = Object.assign(IDONTKNOW.fetchMenu().food, IDONTKNOW.fetchMenu().drink);
       let sum = 0.0;
