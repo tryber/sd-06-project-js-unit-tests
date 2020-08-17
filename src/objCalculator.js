be13f0c6f8bff405c4fd69ea01da2342ccc64a0b
STEFANO-BRANZ <stefano.branz@gmail.com>
2020-08-16 21:24:50 -0300

    Seventh requirement push 02 - code climate fix

--- src/objCalculator.js
+++ src/objCalculator.js
@@ -24,7 +24,7 @@ const calculator = {
 
   mult: (number1, number2) => number1 * number2,
 
-  div:(number1, number2) => Math.floor(number1 / number2),
+  div: (number1, number2) => Math.floor(number1 / number2),
 
   sub: (number1, number2) => number1 - number2,
 };
