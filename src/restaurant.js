7a43b115e0a2b511455b85e9177386549e3c6618
Alvaro Neto <alvaroshn@gmail.com>
2020-08-17 13:52:03 -0300

    Restaurant.js

--- src/restaurant.js
+++ src/restaurant.js
@@ -4,12 +4,12 @@ const createMenu = (meuMenu) => {
   const restaurant = {
     fetchMenu: () => meuMenu,
     consumption: [],
-    order: (stringAsParameter) => {restaurant.consumption.push(stringAsParameter)},
+    order: (stringAsParameter) => { restaurant.consumption.push(stringAsParameter); },
     pay: () => {
       let soma = 0;
-      let menuSize = Object.keys(restaurant.fetchMenu().drink).length + Object.keys(restaurant.fetchMenu().food).length;
-      for (let index = 0; index < menuSize ; index += 1 ){
-        for (let index2 = 0; index2 < restaurant.consumption.length ; index2 += 1 ){
+      const menuSize = Object.keys(restaurant.fetchMenu().drink).length + Object.keys(restaurant.fetchMenu().food).length;
+      for (let index = 0; index < menuSize; index += 1) {
+        for (let index2 = 0; index2 < restaurant.consumption.length; index2 += 1) {
           if (restaurant.consumption[index2] === Object.keys(restaurant.fetchMenu().food)[index]) {
             soma += Object.values(restaurant.fetchMenu().food)[index];
           }
@@ -18,14 +18,14 @@ const createMenu = (meuMenu) => {
           }
         }
       }
-    return soma * 1.1;
+      return soma * 1.1;
     }
   }
   return restaurant;
 };
 
-const objetoInicial = { food: {'coxinha': 3.9, 'sopa': 9.9}, drink: {'agua': 3.9, 'cerveja': 6.9} };
-const meuRestaurante = createMenu (objetoInicial);
+const objetoInicial = { food: {coxinha: 3.9, sopa: 9.9}, drink: {agua: 3.9, cerveja: 6.9 } };
+const meuRestaurante = createMenu(objetoInicial);
 
 const orderFromMenu = (request) => {
   meuRestaurante.order(request);
@@ -37,7 +37,7 @@ orderFromMenu('cerveja');
 orderFromMenu('coxinha');
 orderFromMenu('agua');
 
-console.log('Eu pedi: ' , meuRestaurante.consumption);
-console.log('Minha conta ficou em (acrescidos 10%): ' , meuRestaurante.pay());
+console.log('Eu pedi: ' ,meuRestaurante.consumption);
+console.log('Minha conta ficou em (acrescidos 10%): ' ,meuRestaurante.pay());
 
 module.exports = createMenu;
