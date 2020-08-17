a4d22d752c5b5feb59725a2f49c532dfd541777e
Alvaro Neto <alvaroshn@gmail.com>
2020-08-17 16:19:23 -0300

    restaurant.spec.js

--- src/restaurant.js
+++ src/restaurant.js
@@ -6,19 +6,19 @@ const createMenu = (meuMenu) => {
     consumption: [],
     order: (stringAsParameter) => { restaurant.consumption.push(stringAsParameter); },
     pay: () => {
-      let result = 0;
+      let soma = 0;
       const menuSize = Object.keys(restaurant.fetchMenu().drink).length + Object.keys(restaurant.fetchMenu().food).length;
       restaurant.consumption.forEach((element) => {
         for (let index = 0; index < menuSize; index += 1) {
           if (element === Object.keys(restaurant.fetchMenu().food)[index]) {
-            result += parseFloat(Object.values(restaurant.fetchMenu().food)[index]);
+            soma += parseFloat(Object.values(restaurant.fetchMenu().food)[index]);
           }
           if (element === Object.keys(restaurant.fetchMenu().drink)[index]) {
-            result += parseFloat(Object.values(restaurant.fetchMenu().drink)[index]);
+            soma += parseFloat(Object.values(restaurant.fetchMenu().drink)[index]);
           }
         }
       });
-      return result * 1.1;
+      return soma * 1.1;
     },
   };
   return restaurant;
@@ -37,7 +37,7 @@ orderFromMenu('cerveja');
 orderFromMenu('coxinha');
 orderFromMenu('agua');
 
-// console.log('Eu pedi: ', meuRestaurante.consumption);
-// console.log('Minha conta ficou em (acrescidos 10%): ', meuRestaurante.pay());
+console.log('Eu pedi: ', meuRestaurante.consumption);
+console.log('Minha conta ficou em (acrescidos 10%): ', meuRestaurante.pay());
 
 module.exports = createMenu;
