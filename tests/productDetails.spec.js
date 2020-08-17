05ca30902bb09b796827254d4a27bf38ac24a3bc
DekoPossas <andrepossaster@gmail.com>
2020-08-17 11:05:56 -0300

    i estruturing the assert mode of product function.

--- tests/productDetails.spec.js
+++ tests/productDetails.spec.js
@@ -36,9 +36,21 @@ describe('#productDetails', () => {
     // assert.fail();
     // ESCREVA SEUS TESTES ABAIXO:
     // Teste que o retorno da função é um array.
+    const itens = productDetails('alcool', 'mascara');
+    assert.strictEqual(Array.isArray(itens), true);
     // Teste que o array retornado pela função contém dois itens dentro.
+    assert.strictEqual(itens.length, 2);
     // Teste que os dois itens dentro do array retornado pela função são objetos.
+    assert.strictEqual(typeof itens[1], 'object');
+    assert.strictEqual(typeof itens[0], 'object');
     // Teste que os dois objetos são diferentes entre si.
+    assert.deepEqual((itens[0] !== itens[1]), true);
     // (Difícil) Teste que os dois productIds terminam com 123.
+    const idProduto1 = itens[0].details.productId;
+    const idProduto2 = itens[1].details.productId;
+    const finalString1 = idProduto1.substring(idProduto1.length-3, idProduto1.length);
+    const finalString2 = idProduto2.substring(idProduto2.length-3, idProduto2.length);
+    assert.strictEqual(finalString1, '123');
+    assert.strictEqual(finalString2, '123');
   });
 });
