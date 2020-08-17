da44db6560ebc1992812350e3334e81e520d4904
rodrigoderkian <derkianpromo@gmail.com>
2020-08-17 12:04:54 -0300

    requirement 7

--- src/myCounter.js
+++ src/myCounter.js
@@ -7,10 +7,8 @@
   A função myCounter possui dois loops aninhados que inserem valores dentro de um array.
   Como podemos perceber, eles vão adicionando valores ao array até sua condição de parada.
   Corrija o código abaixo para que a função retorne o array correto.
-
   Parâmetros:
   - Nenhum.
-
   Comportamento:
   myCounter() // Retorna: [0, 2, 3, 1, 2, 3, 2, 2, 3, 3, 2, 3];
 */
@@ -21,9 +19,13 @@ const myCounter = () => {
     myArray.push(counter);
     for (var counter = 2; counter <= 3; counter += 1) {
       myArray.push(counter);
-    }
-  }
-  return myArray;
-};
+      for (let counter1 = 0; counter1 <= 3; counter1 += 1) {
+        myArray.push(counter1);
+        for (counter2 = 2; counter2 <= 3; counter2 += 1) {
+          myArray.push(counter2);
+        }
+      }
+      return myArray;
+    };
 
-module.exports = myCounter;
+    module.exports = myCounter;
