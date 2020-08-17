804b0f493a7b6a5e8a7b38d8521d76d3209b7ce6
DekoPossas <andrepossaster@gmail.com>
2020-08-17 11:15:04 -0300

    codeclimate sofrencia

--- src/productDetails.js
+++ src/productDetails.js
@@ -41,16 +41,18 @@ const productDetails = (firstProduct, secondProduct) => [
   },
 ];
 
-const itens = productDetails('alcool', 'mascara');
+// const itens = productDetails('alcool', 'mascara');
 
 // assert.strictEqual(typeof itens[1], 'object');
 // assert.strictEqual(typeof itens[0], 'object');
 // assert.deepEqual((itens[0] !== itens[1]), true);
 
-const idProduto1 = itens[0].details.productId;
-const idProduto2 = itens[1].details.productId;
-const finalString1 = idProduto1.substring(idProduto1.length-3, idProduto1.length);
-const finalString2 = idProduto2.substring(idProduto2.length-3, idProduto2.length);
+// const idProduto1 = itens[0].details.productId;
+// const idProduto2 = itens[1].details.productId;
+
+// const finalString1 = idProduto1.substring(idProduto1.length -3, idProduto1.length);
+// const finalString2 = idProduto2.substring(idProduto2.length -3, idProduto2.length);
+
 assert.strictEqual(finalString1, '123');
 assert.strictEqual(finalString2, '123');
 
