1885b23e30c4fa9b1013fa6ac35c4ceaf571d7a2
Elisa <elisalibanio1@gmail.comgit config --global user.email elisalibanio1@gmail.com>
2020-08-18 00:57:03 -0300

    5 requisito esta falhando um teste , quero ver se passa os outros ou desconsidera

--- src/createStudent.js
+++ src/createStudent.js
@@ -15,6 +15,11 @@
     estudante.feedback() // Retorna: 'Eita pessoa boa!'
 */
 
-const createStudent = () => {};
-
+const createStudent = (name) => {
+  const estudante = {
+    names: name,
+    feedback: () => 'Eita pessoa boa!',
+  };
+  return estudante.name;
+};
 module.exports = createStudent;
