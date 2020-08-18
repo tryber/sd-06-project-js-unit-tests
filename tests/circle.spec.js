b0c1760eb4e9c0eb9670ad0c503e97cf3c7f32bf
Fabio Camargos <fabiocmmcm@gmail.com>
2020-08-18 18:43:13 -0300

    Bom,  primeiro eu nao sei o POrque mas apenas o deepEqual funciona, depois vou procurar sobre isso

--- tests/circle.spec.js
+++ tests/circle.spec.js
@@ -3,6 +3,7 @@
 
 const assert = require('assert');
 const circle = require('../src/circle');
+const { isObject } = require('util');
 
 /*
   Essa função recebe o raio de um círculo e retorna um objeto contendo suas informações (Raio, Área e Circunferência).
@@ -25,13 +26,19 @@ const circle = require('../src/circle');
 
 describe('#circle', () => {
   it('given a radius, should return an object with circles info', () => {
-    assert.fail();
+    // assert.fail();
     // ESCREVA SEUS TESTES ABAIXO:
     // Teste se circle retorna um objeto.
+    assert.deepEqual(typeof circle(1), 'object')
     // Teste se o objeto retornado tem 3 entradas.
+    assert.deepEqual(circle(1, 7, 3), circle(1, 7, 3))
     // Teste se a função, quando não recebe nenhum parâmetro, retorna undefined.
+    assert.deepEqual(circle(), undefined)
     // Teste que a função retorna, dentro de um objeto, a circunferência correta para um círculo de raio 2.
+    assert.deepEqual(circle(2).circumference, 12.56); //circumference: 2 * PI * radius,
     // Teste que a função retorna, dentro de um objeto, a área correta para um círculo de raio 3.
+    assert.deepEqual(circle(3).area, 28.259999999999998); //area: PI * radius * radius,
     // Teste que a função retorna, num objeto, os dados corretos de um círculo de raio 3.
+    assert.deepEqual(circle(3),{radius: 3, area: 28.259999999999998, circumference: 18.84})
   });
 });
--- tests/productDetails.spec.js
+++ tests/productDetails.spec.js
@@ -3,6 +3,8 @@
 
 const assert = require('assert');
 const productDetails = require('../src/productDetails');
+const { isArray } = require('util');
+const { profileEnd } = require('console');
 
 /*
   Dadas duas strings que representam nomes de produtos, retorne um array contendo dois objetos com os detalhes dos respectivos produtos.
@@ -33,12 +35,18 @@ const productDetails = require('../src/productDetails');
 
 describe('#productDetails', () => {
   it('tests the function has the correct behaviour', () => {
-    assert.fail();
+    // assert.fail();
     // ESCREVA SEUS TESTES ABAIXO:
     // Teste que o retorno da função é um array.
+    assert.strictEqual(Array.isArray(productDetails ('Alcool gel', 'Máscara')), true) 
     // Teste que o array retornado pela função contém dois itens dentro.
+    assert.strictEqual(productDetails('Alcool gel', 'Máscara').length, 2)
     // Teste que os dois itens dentro do array retornado pela função são objetos.
+    assert.strictEqual(typeof(productDetails('Alcool gel', 'Máscara')[0]), 'object')
+    assert.strictEqual(typeof(productDetails('Alcool gel', 'Máscara')[1]), 'object')
     // Teste que os dois objetos são diferentes entre si.
+    assert.notStrictEqual(productDetails('Alcool gel', 'Máscara')[0], productDetails('Alcool gel', 'Máscara')[1])
     // (Difícil) Teste que os dois productIds terminam com 123.
+
   });
 });
