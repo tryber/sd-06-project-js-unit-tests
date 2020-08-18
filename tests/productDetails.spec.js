0d4203909c320f7acff22d06dd135d327ea181ec
Elisa <elisalibanio1@gmail.comgit config --global user.email elisalibanio1@gmail.com>
2020-08-17 23:58:48 -0300

    productDetais tests

--- tests/productDetails.spec.js
+++ tests/productDetails.spec.js
@@ -33,12 +33,17 @@ const productDetails = require('../src/productDetails');
 
 describe('#productDetails', () => {
   it('tests the function has the correct behaviour', () => {
-    assert.fail();
-    // ESCREVA SEUS TESTES ABAIXO:
-    // Teste que o retorno da função é um array.
+    // assert.fail();
+    assert.deepEqual(typeof(productDetails()), 'object');
     // Teste que o array retornado pela função contém dois itens dentro.
+    assert(Object.keys.length, 2);
     // Teste que os dois itens dentro do array retornado pela função são objetos.
+    assert(typeof(productDetails()[0]&& [1],'object'))
     // Teste que os dois objetos são diferentes entre si.
+    assert.deepStrictEqual(productDetails()[0], productDetails()[1]);
+
     // (Difícil) Teste que os dois productIds terminam com 123.
+    assert.strictEqual(productDetails('Alcool gel', 'Máscara')[0].details.productId.endsWith('123'), true);
+    assert.strictEqual(productDetails('Alcool gel', 'Máscara')[1].details.productId.endsWith('123'), true);
   });
 });
