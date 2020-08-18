4fbb1cbf02c85906aa1fd8b809e83452100d93ab
Pedro Duarte <ikbenpedro@gmail.com>
2020-08-18 14:20:38 -0300

    setimo requisito / code climate 1.4

--- src/createStudent.js
+++ src/createStudent.js
@@ -14,20 +14,20 @@
     estudante.name // Retorna: 'Leandrão, o Lobo Solitário'
     estudante.feedback() // Retorna: 'Eita pessoa boa!'
 */
-const assert = require('assert');
+// const assert = require('assert');
 
 const createStudent = (nameOfStudent) => {
- const student = {
+  const student = {
    name: nameOfStudent,
    feedback: () => 'Eita pessoa boa!',
-  };
+};
   return student;
 };
 //  console.log(createStudent('Leandrão, o Lobo Solitário'))
 
- const estudante = createStudent('Leandrão, o Lobo Solitário');
+// const estudante = createStudent('Leandrão, o Lobo Solitário');
 // assert.strictEqual(typeof estudante, 'object');
- assert.strictEqual(typeof estudante.feedback, 'function');
+// assert.strictEqual(typeof estudante.feedback, 'function');
 // assert.strictEqual(estudante.name, 'Leandrão, o Lobo Solitário');
 // assert.strictEqual(estudante.feedback(), 'Eita pessoa boa!');
 
