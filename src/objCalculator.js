0593f09280ee997d23ceb1b2d35ca9673c53c78f
DekoPossas <andrepossaster@gmail.com>
2020-08-19 12:50:19 -0300

    comentei os testes!

--- src/objCalculator.js
+++ src/objCalculator.js
@@ -19,7 +19,7 @@
   calculator.div(3, 2) // Retorno: 1;
 */
 
-const assert = require('assert');
+// const assert = require('assert');
 
 const calculator = {
   add: (a, b) => a + b,
@@ -31,15 +31,15 @@ const calculator = {
   sub: (a, b) => a - b,
 };
 
-assert.strictEqual(calculator.add(1, 3), 4);
-assert.strictEqual(calculator.add(2, 9), 11);
-assert.strictEqual(calculator.mult(10, 3), 30);
-assert.strictEqual(calculator.mult(4, 7), 28);
-assert.strictEqual(calculator.div(5, 2), 2);
-assert.strictEqual(calculator.div(7, 2), 3);
-assert.strictEqual(calculator.sub(1, 3), -2);
-assert.strictEqual(calculator.sub(1, 9), -8);
+// assert.strictEqual(calculator.add(1, 3), 4);
+// assert.strictEqual(calculator.add(2, 9), 11);
+// assert.strictEqual(calculator.mult(10, 3), 30);
+// assert.strictEqual(calculator.mult(4, 7), 28);
+// assert.strictEqual(calculator.div(5, 2), 2);
+// assert.strictEqual(calculator.div(7, 2), 3);
+// assert.strictEqual(calculator.sub(1, 3), -2);
+// assert.strictEqual(calculator.sub(1, 9), -8);
 
-console.log(typeof calculator.add);
+// console.log(typeof calculator.add);
 
 module.exports = calculator;
