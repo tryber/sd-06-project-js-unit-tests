278dc03f847517fb837fe0961276ca4a91728da4
ilanribeiro <ilanribeiro@gmail.com>
2020-08-19 12:36:42 -0300

    requirement 9 under construction. tests 1 and 2, complete. function step 1, complete - [restaurant.js and restaurant.spec.js]-v.2(codeclimate)

--- src/restaurant.js
+++ src/restaurant.js
@@ -72,8 +72,6 @@
 // PASSO 4: Adicione ao objeto retornado por `createMenu()` uma chave `pay` com uma função que varre todo os itens de `objetoRetornado.consumption`, soma o preço de todos checando-os no menu e retorna o valor somado acrescido de 10%. DICA: para isso, você precisará varrer tanto o objeto da chave `food` quanto o objeto da chave `drink`.
 
 // BUILDING FUNCTIONS
-const createMenu = (objetoQualquer) => {
-  return { fetchMenu: objetoQualquer }
-};
+const createMenu = (objetoQualquer) => ({ fetchMenu: objetoQualquer });
 
 module.exports = createMenu;
