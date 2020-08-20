eda232bdf809a81f076ecea737f19106b13fef48
Alana Monteiro <lanaah.15@gmail.com>
2020-08-20 13:01:11 -0300

    objCalculator ok

--- src/objCalculator.js
+++ src/objCalculator.js
@@ -19,6 +19,11 @@
   calculator.div(3, 2) // Retorno: 1;
 */
 
-const calculator = {};
+const calculator = {
+  add: adicao = (number1, number2) => Math.floor(number1 + number2),
+    mult: multipicacao = (number1, number2) => Math.floor(number1 * number2),
+  div: divisao = (number1, number2) => Math.floor(number1 / number2),
+  sub: subtracao = (number1, number2) => Math.floor(number1 - number2),
+};
 
 module.exports = calculator;
