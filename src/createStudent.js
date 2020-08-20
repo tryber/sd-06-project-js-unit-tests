cf156ed27d4d52d3fa9e7b6e2091afc87396f8df
Danilo <dansantosalfa@gmail.com>
2020-08-19 23:17:41 -0300

    Quinta questão

--- src/createStudent.js
+++ src/createStudent.js
@@ -15,6 +15,14 @@
     estudante.feedback() // Retorna: 'Eita pessoa boa!'
 */
 
-const createStudent = () => {};
+const createStudent = (student) => {
+  const fix = {
+    name: student,
+    feedback() {
+      return 'Eita pessoa boa!';
+    },
+  };
+  return fix;
+};
 
 module.exports = createStudent;
