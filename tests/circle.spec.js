220b361f1a842226bb3fd480b0b18ff18f26ed8b
Victor Bergamini <vbergadev@gmail.com>
2020-08-20 18:14:18 -0300

    circle, colocando valores completos para ver se vai

--- tests/circle.spec.js
+++ tests/circle.spec.js
@@ -26,9 +26,9 @@ const circle = require('../src/circle');
 describe('#circle', () => {
   it('given a radius, should return an object with circles info', () => {
     // ESCREVA SEUS TESTES ABAIXO:
-    assert.strictEqual(typeof (circle(1)), 'object');
-    assert.strictEqual(Object.keys(circle(1)).length, 3)
-    assert.strictEqual(circle(), 'undefined');
+    assert.deepStrictEqual(typeof (circle(1)), 'object');
+    assert.deepStrictEqual(Object.keys(circle(1)).length, 3)
+    assert.deepStrictEqual(circle(), undefined);
     assert.deepStrictEqual(circle(2).circumference, 12.56);
     assert.deepStrictEqual(circle(3).area, 28.259999999999998);
     assert.deepStrictEqual(Object.values(circle(3)), [3, 28.259999999999998, 18.84]);
