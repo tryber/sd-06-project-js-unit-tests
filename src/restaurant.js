4bd994454d8eb82bb63f702bd4398be93ec6be44
Cleyton Oliveira <cleytonoliveirapro@gmail.com>
2020-08-20 03:51:18 -0300

    Add test and function implementation of restaurant

--- src/restaurant.js
+++ src/restaurant.js
@@ -71,6 +71,30 @@
 
 // PASSO 4: Adicione ao objeto retornado por `createMenu()` uma chave `pay` com uma função que varre todo os itens de `objetoRetornado.consumption`, soma o preço de todos checando-os no menu e retorna o valor somado acrescido de 10%. DICA: para isso, você precisará varrer tanto o objeto da chave `food` quanto o objeto da chave `drink`.
 
-const createMenu = () => {};
+const createMenu = (menu) => {
+  const myRestaurant = {
+    fetchMenu: () => menu,
+    consumption: [],
+    order: (item) => {
+      myRestaurant.consumption.push(item);
+    },
+    pay: () => {
+      let sumOfOrdersConsumption = 0;
+      myRestaurant.consumption.forEach((item) => {
+        for (let count = 0; count < Object.keys(myRestaurant.fetchMenu()).length; count += 1) {
+          if (Object.keys(myRestaurant.fetchMenu().food)[count] === item) {
+            sumOfOrdersConsumption += Object.values(myRestaurant.fetchMenu().food)[count];
+          }
+          if (Object.keys(myRestaurant.fetchMenu().drink)[count] === item) {
+            sumOfOrdersConsumption += Object.values(myRestaurant.fetchMenu().drink)[count];
+          }
+        }
+      });
+      const taxService = Number((sumOfOrdersConsumption * 1.1).toFixed(2));
+      return taxService;
+    },
+  };
+  return myRestaurant;
+};
 
 module.exports = createMenu;
