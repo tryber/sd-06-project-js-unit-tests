a315ea5f58efbda91c893863c1d879ef89963f1f
Victor Bergamini <vbergadev@gmail.com>
2020-08-20 15:58:17 -0300

    code numbers

--- src/myCounter.js
+++ src/myCounter.js
@@ -16,11 +16,11 @@
 */
 
 const myCounter = () => {
-  var myArray = [];
-  for (var counter = 0; counter <= 3; counter += 1) {
-    myArray.push(counter);
-    for (var counter = 2; counter <= 3; counter += 1) {
-      myArray.push(counter);
+  let myArray = [];
+  for (let i = 0; i <= 3; i += 1) {
+    myArray.push(i);
+    for (let i = 2; i <= 3; i += 1) {
+      myArray.push(i);
     }
   }
   return myArray;
