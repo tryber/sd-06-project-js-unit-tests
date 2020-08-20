00f62cf5f04ec288f4c119cbf2938676f0beef9b
Victor Bergamini <vbergadev@gmail.com>
2020-08-20 16:00:46 -0300

    code climate myCounter e  numbers

--- src/myCounter.js
+++ src/myCounter.js
@@ -16,10 +16,10 @@
 */
 
 const myCounter = () => {
-  let myArray = [];
-  for (let i = 0; i <= 3; i += 1) {
+  const myArray = [];
+  for (const i = 0; i <= 3; i += 1) {
     myArray.push(i);
-    for (let i = 2; i <= 3; i += 1) {
+    for (const i = 2; i <= 3; i += 1) {
       myArray.push(i);
     }
   }
