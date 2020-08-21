ebe463794f7ec88b347d1629b1167faba9238155
Paulo Frazão <paulo4187@gmail.com>
2020-08-21 10:13:23 -0300

    requisito mycounter codeC

--- src/myCounter.js
+++ src/myCounter.js
@@ -16,7 +16,7 @@
 */
 
 const myCounter = () => {
-  let myArray = [];
+  const myArray = [];
   for (var counter = 0; counter <= 3; counter += 1) {
     myArray.push(counter);
     for (var index = 2; index <= 3; index += 1) {
