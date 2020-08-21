ef9d096767be2aa6314776e24653cc27677f11f9
IzeldaValeriano <zel.valeriano@gmail.com>
2020-08-21 10:46:15 -0300

    vqv

--- src/myCounter.js
+++ src/myCounter.js
@@ -18,10 +18,16 @@
 const myCounter = () => {
   var myArray = [];
   for (var counter = 0; counter <= 3; counter += 1) {
-    myArray.push(counter);
-    for (var counter = 2; counter <= 3; counter += 1) {
-      myArray.push(counter);
-    }
+    const myArray = [];
+   for (let counter = 0; counter <= 3; counter += 1) {
+ myArray.push(counter);
+ for (let counter = 2; counter <= 3; counter ++) {
+   myArray.push(counter);
+   for (let counter2 = 2; counter2 <= 3; counter2 ++) {
+     myArray.push(counter2);
+   }
+ }
+   }
   }
   return myArray;
 };
