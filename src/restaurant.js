413d3a613d03b29ff4af57a60d7d1e73dcf48b91
paulo-lins <paulolins100@gmail.com>
2020-08-21 11:29:16 -0300

    primeiro teste requiseito 9

--- src/restaurant.js
+++ src/restaurant.js
@@ -46,8 +46,8 @@
 // PASSO 1: Crie uma função `createMenu()` que, dado um objeto passado por parâmetro, retorna um objeto com o seguinte formato: { fetchMenu: objetoPassadoPorParametro }.
 
 function createMenu(obj) {
-  return fetchMenu(obj)
-}
+  return fetchMenu(obj);
+};
 //
 // Agora faça o TESTE 2 no arquivo `tests/restaurant.spec.js`.
 
@@ -74,14 +74,13 @@ function createMenu(obj) {
 //------------------------------------------------------------------------------------------
 
 // PASSO 4: Adicione ao objeto retornado por `createMenu()` uma chave `pay` com uma função que varre todo os itens de `objetoRetornado.consumption`, soma o preço de todos checando-os no menu e retorna o valor somado acrescido de 10%. DICA: para isso, você precisará varrer tanto o objeto da chave `food` quanto o objeto da chave `drink`.
+// ------------------------->>>>
 // const menu = {
-//   food: 'coxinha', 
-//   drink: 'agua', 
+//   food: 'coxinha',
+//   drink: 'agua',
 // }
 // const createMenu = (obj) => {
 //   fetchMenu: obj,
-  
-
 // };
 // console.log(createMenu(menu))
 module.exports = createMenu;
@@ -92,4 +91,4 @@ module.exports = createMenu;
     // createMenu(objetoQualquer) // Retorno: { fetchMenu: objetoQualquer }
     // ```
     // Agora faça o PASSO 1 no arquivo `src/restaurant.js`.
-    // --------------------------------------------------------------------------------------
+    // 
