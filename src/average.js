52b4fc9c0c89bab38e3e01543f4d0b70419d4ff7
IzeldaValeriano <zel.valeriano@gmail.com>
2020-08-23 16:08:46 -0300

    correção requisito average

--- src/average.js
+++ src/average.js
@@ -15,8 +15,8 @@
 const average = (numbers) => {
   let sum = 0;
   let media = 0;
-  if(numbers.length === 0){ 
-     return undefined;
+  if (numbers.length === 0) { 
+    return undefined;
   }
   for (let i = 0; i < numbers.length; i += 1) {
     if (typeof numbers[i] !== 'number') {
