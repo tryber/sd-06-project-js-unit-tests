3214d8e7274659aab43989b0cb67c23d09ad2af3
IzeldaValeriano <zel.valeriano@gmail.com>
2020-08-26 09:07:56 -0300

    corrigindo requisito 9 codeClimate2

--- src/restaurant.js
+++ src/restaurant.js
@@ -47,10 +47,6 @@
 
 // Agora faça o TESTE 2 no arquivo `tests/restaurant.spec.js`.
 
-//------------------------------------------------------------------ivo `tests/restaurant.spec.js`.
-
-//------------------------------------------------------------------------------------------
-
 // PASSO 4: Adicione ao objeto retornado por `createMenu()` uma chave `pay` com uma função que varre todo os itens de `objetoRetornado.consumption`, soma o preço de todos checando-os no menu e retorna o valor somado acrescido de 10%. DICA: para isso, você precisará varrer tanto o objeto da chave `food` quanto o objeto da chave `drink`.
 
 const createMenu = () => {
