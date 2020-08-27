367c6c6b71f3238b43e664ca9b0ee298304721d1
IzeldaValeriano <zel.valeriano@gmail.com>
2020-08-27 15:12:32 -0300

    refatorando requito

--- src/restaurant.js
+++ src/restaurant.js
@@ -49,7 +49,11 @@
 
 // PASSO 4: Adicione ao objeto retornado por `createMenu()` uma chave `pay` com uma função que varre todo os itens de `objetoRetornado.consumption`, soma o preço de todos checando-os no menu e retorna o valor somado acrescido de 10%. DICA: para isso, você precisará varrer tanto o objeto da chave `food` quanto o objeto da chave `drink`.
 
-const createMenu = () => {}
+const createMenu = (objectmenu) => {
+    const menu = {}
+    menu.fetchMenu = objectmenu;
+    return objectmenu;
+}
 module.exports = createMenu;
 
 // PASSO 2: Adicione ao objeto retornado por `createMenu` uma chave `consumption` que, como valor inicial, tem um array vazio.
--- tests/restaurant.spec.js
+++ tests/restaurant.spec.js
@@ -3,6 +3,7 @@
 
 const assert = require('assert');
 const createMenu = require('../src/restaurant');
+const { deepEqual } = require('assert');
 
 /*
   Você é responsável por escrever o código do sistema de pedidos de um restaurante. Deve ser possível, através desse sistema, cadastrar um menu. Dado que um menu foi cadastrado, o sistema deve disponibilizar um objeto através do qual se consegue:
@@ -51,12 +52,14 @@ const createMenu = require('../src/restaurant');
 
 describe('#createMenu', () => {
   it('tests the function has the correct behaviour', () => {
-    assert.fail();
-    // TESTE 1: Verifique que, dado um objeto qualquer passado como um parâmetro para a função createMenu(), checa se o retorno da função é um objeto no seguinte formato: { fetchMenu: objetoQualquer }.
-    // ```
+    //assert.fail();
+     //TESTE 1: Verifique que, dado um objeto qualquer passado como um parâmetro para a função createMenu(), checa se o retorno da função é um objeto no seguinte formato: { fetchMenu: objetoQualquer }.
+     const object = { food: {}, drink: {}};
+     const retorn = createMenu(object);
+    assert.deepStrictEqual(createMenu(object),object);
     // createMenu(objetoQualquer) // Retorno: { fetchMenu: objetoQualquer }
     // Agora faça o PASSO 1 no arquivo `src/restaurant.js`.
-    // --------------------------------------------------------------------------------------
+    // ------------------------------------------------------------------------------------
     // TESTE 2: Verifique que, dado que a função createMenu foi chamada com o objeto: `{ food: {}, drink: {} }`, verifique que 'objetoRetornado.fetchMenu' retorna um objeto cujas chaves são somente `food` e `drink`.
     // const objetoRetornado = createMenu(objetoQualquer);
     // objetoRetornado.fetchMenu // Retorno: { food: {}, drink: {}}
