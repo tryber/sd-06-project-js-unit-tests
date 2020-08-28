5297ce8268930dc59dbfa0cf283f4a039ce5a1b1
IzeldaValeriano <zel.valeriano@gmail.com>
2020-08-27 21:03:22 -0300

    codeClimate3

--- src/restaurant.js
+++ src/restaurant.js
@@ -57,15 +57,24 @@ const createMenu = (objectmenu) => {
   };
   menu.pay = () => {
     let sum = 0;
-    menu.consumption.forEach(element => {
-    switch (element) {
-        case 'coxinha': sum += menu.fetchMenu.food.coxinha; break;
-        case 'sanduiche': sum += menu.fetchMenu.food.sanduiche; break;
-        case 'agua' : sum += menu.fetchMenu.drinks.agua; break;
-        case 'cerveja': sum += menu.fetchMenu.drinks.cerveja; break;
-        default: break;
+    menu.consumption.forEach((element) => {
+      switch (element) {
+        case "coxinha":
+          sum += menu.fetchMenu.food.coxinha;
+          break;
+        case "sanduiche":
+          sum += menu.fetchMenu.food.sanduiche;
+          break;
+        case "agua":
+          sum += menu.fetchMenu.drinks.agua;
+          break;
+        case "cerveja":
+          sum += menu.fetchMenu.drinks.cerveja;
+          break;
+        default:
+          break;
       }
-  });
+    });
     return (sum * 1.1).toFixed(2);
   };
   return menu;
